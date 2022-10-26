f04abfc514582c282e8780f9d477d0eb376861f0
Leonel Sanches da Silva <leonel@designliquido.com.br>
2022-10-26 14:22:04 -0700

    Melhorando o teste do Lexador.

--- testes/lexador.test.ts
+++ testes/lexador.test.ts
@@ -1,4 +1,5 @@
 import { Lexador } from "../fontes/lexador";
+import tiposDeSimbolos from "../fontes/tipos-de-simbolos";
 
 describe('Lexador', () => {
     describe('mapear()', () => {
@@ -15,6 +16,19 @@ describe('Lexador', () => {
                 "}"
             ]);
             expect(resultado).toBeTruthy();
+            expect(resultado.simbolos).toHaveLength(8);
+            expect(resultado.simbolos).toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({ tipo: tiposDeSimbolos.ESTRUTURA }),
+                    expect.objectContaining({ tipo: tiposDeSimbolos.CHAVE_ESQUERDA }),
+                    expect.objectContaining({ tipo: tiposDeSimbolos.IDENTIFICADOR }),
+                    expect.objectContaining({ tipo: tiposDeSimbolos.DOIS_PONTOS }),
+                    expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
+                    expect.objectContaining({ tipo: tiposDeSimbolos.IDENTIFICADOR }),
+                    expect.objectContaining({ tipo: tiposDeSimbolos.PONTO_E_VIRGULA }),
+                    expect.objectContaining({ tipo: tiposDeSimbolos.CHAVE_DIREITA }),
+                ])
+            );
         });
     });
 });
