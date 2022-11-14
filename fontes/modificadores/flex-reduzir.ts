5a3dc72d34f9d601d82e62a3a9b26fcb57a7fca1
VitBrandao <vitorsbsilva@gmail.com>
2022-11-14 15:34:26 -0300

    Consertando modificadores com erro de digitação

--- fontes/modificadores/flex-reduzir.ts
+++ fontes/modificadores/flex-reduzir.ts
@@ -1,6 +1,6 @@
 import { Modificador } from "./superclasse/modificador";
 
-export class Flexeduzir extends Modificador {
+export class FlexReduzir extends Modificador {
     constructor(valor: string, quantificador: string) {
         super("flex-reduzir", "flex-shrink");
         this.valor = valor;
