043a7df06318d0ab9990e89aa80b03e9f3b4360a
VitBrandao <vitorsbsilva@gmail.com>
2022-11-17 15:00:09 -0300

    Refatorando classe dinâmica para receber valor e quantificador

--- fontes/modificadores/superclasse/seletor-modificador.ts
+++ fontes/modificadores/superclasse/seletor-modificador.ts
@@ -1,13 +1,13 @@
 import { DicionarioModificadores } from "../dicionario/dicionario-modificadores";
 
 export class SeletorModificador {
-    constructor(nomeFolEs: string) {
+    constructor(nomeFolEs: string, valor: string, quantificador: string) {
         if (
             DicionarioModificadores[nomeFolEs] === undefined || 
             DicionarioModificadores[nomeFolEs] === null
         ) {
             throw new Error(`O seletor \'${nomeFolEs}\' não foi encontrado.`);
         }
-        return new DicionarioModificadores[nomeFolEs];
+        return new DicionarioModificadores[nomeFolEs](valor, quantificador);
     }
 }
