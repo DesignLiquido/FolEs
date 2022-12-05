59926f634b81607e74832490bb0d4bcc485c94b1
VitBrandao <vitorsbsilva@gmail.com>
2022-12-05 09:29:30 -0300

    Simplificando testes - Cor, Estilo, Posição e Status

--- testes/modificadores/status.test.ts
+++ testes/modificadores/status.test.ts
@@ -38,24 +38,10 @@ describe('Testando Seletores com STATUS como atributo', () => {
                     "}"
                 ]);
 
-                expect(resultadoLexador).toBeTruthy();
                 expect(resultadoLexador.simbolos).toHaveLength(7);
                 expect(resultadoLexador.simbolos).toEqual(
                     expect.arrayContaining([
-                        expect.objectContaining({ tipo: tiposDeSimbolos.ESTRUTURA }),
-                        expect.objectContaining({ tipo: tiposDeSimbolos.CHAVE_ESQUERDA }),
-                        expect.objectContaining({ tipo: tiposDeSimbolos.IDENTIFICADOR }),
-                        expect.objectContaining({ tipo: tiposDeSimbolos.DOIS_PONTOS }),
                         // expect.objectContaining({ tipo: tiposDeSimbolos.ATRIBUTO_STATUS }),
-                        expect.objectContaining({ tipo: tiposDeSimbolos.PONTO_E_VIRGULA }),
-                        expect.objectContaining({ tipo: tiposDeSimbolos.CHAVE_DIREITA }),
-                    ])
-                );
-
-                expect(resultadoLexador.simbolos).not.toEqual(
-                    expect.arrayContaining([
-                        expect.objectContaining({ tipo: tiposDeSimbolos.QUANTIFICADOR }),
-                        expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
                     ])
                 );
 
@@ -63,27 +49,18 @@ describe('Testando Seletores com STATUS como atributo', () => {
                 // Avaliador Sintático
                 const resultadoAvaliadorSintatico = avaliador.analisar(resultadoLexador.simbolos);
 
-                expect(resultadoAvaliadorSintatico).toBeTruthy();
-                expect(resultadoAvaliadorSintatico).toHaveLength(1);
-                expect(resultadoAvaliadorSintatico[0].seletor).toBe('corpo');
                 expect(resultadoAvaliadorSintatico[0].modificadores[0].nomeFoles).toStrictEqual(
                     seletor['nomeFoles']
                 );
                 expect(resultadoAvaliadorSintatico[0].modificadores[0].propriedadeCss).toStrictEqual(
                     seletor['propriedadeCss']
                 );
-                expect(resultadoAvaliadorSintatico[0].modificadores[0].valor).toStrictEqual(
-                    'sempre'
-                );
-
 
                 // Tradutor
                 const resultadoTradutor = tradutor.traduzir(resultadoAvaliadorSintatico);
 
-                expect(resultadoTradutor).toBeTruthy();
-                expect(resultadoTradutor).toContain("body");
                 expect(resultadoTradutor).toContain(seletor['propriedadeCss']);
-                expect(resultadoTradutor).toContain('always;');
+                expect(resultadoTradutor).toContain('always');
             }
         });
 
