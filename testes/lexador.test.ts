b6ca7a04f48d9dc20c2b59111f4c1eeec5ebdb08
Leonel Sanches da Silva <leonel@designliquido.com.br>
2022-12-15 09:14:29 -0800

    Removendo `describe()`s desnecessários.

--- testes/lexador.test.ts
+++ testes/lexador.test.ts
@@ -1,74 +1,73 @@
 import { Lexador } from "../fontes/lexador";
+
 import tiposDeSimbolos from "../fontes/tipos-de-simbolos/foles";
 
-describe('Testes Unitários', () => {
-    describe('Lexador', () => {
-        let lexador: Lexador;
+describe('Lexador', () => {
+    let lexador: Lexador;
 
-        beforeEach(() => {
-            lexador = new Lexador();
-        });
+    beforeEach(() => {
+        lexador = new Lexador();
+    });
 
-        it('Caso de sucesso - função mapear() monta objeto corretamente', () => {
-            const resultadoLexador = lexador.mapear([
-                "lmht {",
-                "    tamanho-texto: 12px;",
-                "}"
-            ]);
+    it('Caso de sucesso - função mapear() monta objeto corretamente', () => {
+        const resultadoLexador = lexador.mapear([
+            "lmht {",
+            "    tamanho-texto: 12px;",
+            "}"
+        ]);
 
-            expect(resultadoLexador).toBeTruthy();
-            expect(resultadoLexador.simbolos).toHaveLength(8);
+        expect(resultadoLexador).toBeTruthy();
+        expect(resultadoLexador.simbolos).toHaveLength(8);
 
-            // Tipos devem estar mapeados
-            expect(resultadoLexador.simbolos).toEqual(
-                expect.arrayContaining([
-                    expect.objectContaining({ tipo: tiposDeSimbolos.ESTRUTURA }),
-                    expect.objectContaining({ tipo: tiposDeSimbolos.CHAVE_ESQUERDA }),
-                    expect.objectContaining({ tipo: tiposDeSimbolos.IDENTIFICADOR }),
-                    expect.objectContaining({ tipo: tiposDeSimbolos.DOIS_PONTOS }),
-                    expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
-                    expect.objectContaining({ tipo: tiposDeSimbolos.QUANTIFICADOR }),
-                    expect.objectContaining({ tipo: tiposDeSimbolos.PONTO_E_VIRGULA }),
-                    expect.objectContaining({ tipo: tiposDeSimbolos.CHAVE_DIREITA }),
-                ])
-            );
+        // Tipos devem estar mapeados
+        expect(resultadoLexador.simbolos).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ tipo: tiposDeSimbolos.ESTRUTURA }),
+                expect.objectContaining({ tipo: tiposDeSimbolos.CHAVE_ESQUERDA }),
+                expect.objectContaining({ tipo: tiposDeSimbolos.IDENTIFICADOR }),
+                expect.objectContaining({ tipo: tiposDeSimbolos.DOIS_PONTOS }),
+                expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
+                expect.objectContaining({ tipo: tiposDeSimbolos.QUANTIFICADOR }),
+                expect.objectContaining({ tipo: tiposDeSimbolos.PONTO_E_VIRGULA }),
+                expect.objectContaining({ tipo: tiposDeSimbolos.CHAVE_DIREITA }),
+            ])
+        );
 
-            // Lexemas devem estar mapeados
-            expect(resultadoLexador.simbolos).toEqual(
-                expect.arrayContaining([
-                    expect.objectContaining({ lexema: '12' }),
-                    expect.objectContaining({ lexema: 'px' }),
-                ])
-            );
-        });
+        // Lexemas devem estar mapeados
+        expect(resultadoLexador.simbolos).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ lexema: '12' }),
+                expect.objectContaining({ lexema: 'px' }),
+            ])
+        );
+    });
 
-        it('Caso de falha - mapear() não recebe parâmetros corretos', () => {
-            const resultadoLexador = lexador.mapear([
-                "lmht {",
-                "    tamanho-texto: px;",
-                "}"
-            ]);
+    it('Caso de falha - mapear() não recebe parâmetros corretos', () => {
+        const resultadoLexador = lexador.mapear([
+            "lmht {",
+            "    tamanho-texto: px;",
+            "}"
+        ]);
 
-            // Não deve mapear números
-            expect(resultadoLexador.simbolos).not.toEqual(
-                expect.arrayContaining([
-                    expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
-                ])
-            );
+        // Não deve mapear números
+        expect(resultadoLexador.simbolos).not.toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
+            ])
+        );
 
-            const novoLexador = lexador.mapear([
-                "lmht {",
-                "    tamanho-texto: 12;",
-                "}"
-            ]);
+        const novoLexador = lexador.mapear([
+            "lmht {",
+            "    tamanho-texto: 12;",
+            "}"
+        ]);
 
 
-            // Não deve mapear quantificador
-            expect(novoLexador.simbolos).not.toEqual(
-                expect.arrayContaining([
-                    expect.objectContaining({ tipo: tiposDeSimbolos.QUANTIFICADOR }),
-                ])
-            );
-        });
+        // Não deve mapear quantificador
+        expect(novoLexador.simbolos).not.toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({ tipo: tiposDeSimbolos.QUANTIFICADOR }),
+            ])
+        );
     });
 });
