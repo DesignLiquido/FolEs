4b44a44dc091b3ccd8941e0be4d0b141f6f0d206
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2022-12-15 11:15:41 -0800

    Exemplo de validação em `direcao-animacao`. (#23)

--- fontes/modificadores/direcao-animacao.ts
+++ fontes/modificadores/direcao-animacao.ts
@@ -1,12 +1,25 @@
 import { Modificador } from "./superclasse/modificador";
 
 export class DirecaoAnimacao extends Modificador {
+    valoresAceitos: { [valorFoles: string]: string } = {
+        "normal": "normal",
+        "reverter": "reverse",
+        "alternar": "alternate",
+        "alternar-reverter": "alternate-reverse"
+    }
+
     constructor(valor: string, quantificador: string) {
         super(
             ["direcao-animacao", "direção-animação"], 
             "animation-direction"
         );
+
+        if (!(valor in this.valoresAceitos)) {
+            throw new Error(`Valor ${valor} inválido para 'direção-animação'. Valores aceitos: ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += atual)}`);
+        }
+
         this.valor = valor;
-        this.quantificador = quantificador;
+        // Quantificador não é usado aqui.
+        // this.quantificador = quantificador;
     }
 }
