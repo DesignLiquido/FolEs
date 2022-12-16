9b15a67a9442a10cc6e62ec9a83c9b51b99731cb
VitBrandao <vitorsbsilva@gmail.com>
2022-12-16 12:36:16 -0300

    Adiciona valores possiveis para os modificadores Animation

--- fontes/modificadores/duracao-animacao.ts
+++ fontes/modificadores/duracao-animacao.ts
@@ -6,7 +6,17 @@ export class DuracaoAnimacao extends Modificador {
             ["duracao-animacao", "duração-animação"], 
             "animation-duration"
         );
+
+        if (Number.isNaN(parseInt(valor))) {
+            throw new Error(`Propriedade 'duração-animação' com valor ${valor} inválido. Valor deve ser um número`)
+        }
+
         this.valor = valor;
+
+        if (!['s', 'ms'].includes(quantificador)) {
+            throw new Error(`Propriedade 'duração-animação' com quantificador ${quantificador} inválido. Valores aceitos: 's', 'ms'`);
+        }
+
         this.quantificador = quantificador;
     }
 }
---
+++ fontes/modificadores/velocidade-animacao.ts
@@ -0,0 +1,62 @@
+import { Modificador } from "./superclasse/modificador";
+
+export class VelocidadeAnimacao extends Modificador {
+    valoresAceitos: { [valorFoles: string]: string } = {
+        "velocidade-normal": "ease",
+        "linear": "linear",
+        "inicio-lento": "ease-in",
+        "início-lento": "ease-in",
+        "final-lento": "ease-out",
+        "inicio-final-lento": "ease-in-out",
+        "início-final-lento": "ease-in-out",
+        "curva-cubica": "cubic-bezier",
+        "curva-cúbica": "cubic-bezier",
+        "passos": "steps"
+    }
+
+    termosSalto: { [valorFoles: string]: string } = {
+        "salto-inicial": "jump-start",
+        "salto-final": "jump-end",
+        "salto-nenhum": "jump-none",
+        "salto-conjunto": "jump-both",
+        "inicial": "start",
+        "final": "end",
+        "passo-inicial": "step-start",
+        "passo-final": "step-start"
+    }
+
+    constructor(valor: string, quantificador?: string) {
+        super(
+            ["velocidade-animacao", "velocidade-animação"],
+            "animation-timing-function"
+        );
+
+        // Além dos valores aceitos, o modificador também pode receber:
+
+        // cubic-bezier(p1, p2, p3, p4) -> curva-cúbica, que recebe 4 números como parâmetro,
+        // onde p1 e p3 devem estar na faixa de 0 a 1.
+        // Ex.: curva-cúbica(1, 5, 0, 6);
+
+        if (['curva-cubica', 'curva-cúbica'].includes(valor)){
+            // Lógica para validar 4 parâmetros
+        }
+
+        // steps(n, <jumpterm>) -> passos, que recebe dois parâmetros: um número e um termo de salto.
+        // O número indica quantas pausas terão entre os saltos. O termo deve ser um dos listados em termosSalto.
+        // Ex.: passos(3, salto-inicial);
+
+        if (['passos'].includes(valor)){
+            // Lógica para validar 2 parâmetros
+        }
+        
+        // Demais valores aceitos
+        if (!(valor in this.valoresAceitos)) {
+            throw new Error(`Valor ${valor} inválido para 'velocidade-animação'. Valores aceitos: ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += `, ${atual}`)}.`);
+        }
+
+        this.valor = valor;
+
+        // Não recebe quantificador
+        // this.quantificador = quantificador;
+    }
+}
