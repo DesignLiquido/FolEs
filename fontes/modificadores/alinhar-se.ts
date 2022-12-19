410b4526c7c36678b7a822757c2bb69d09291735
VitBrandao <vitorsbsilva@gmail.com>
2022-12-19 14:07:55 -0300

    Adicionando valores aos 3 modificadores 'Alinhar'

--- fontes/modificadores/alinhar-se.ts
+++ fontes/modificadores/alinhar-se.ts
@@ -1,9 +1,44 @@
 import { Modificador } from "./superclasse/modificador";
 
 export class AlinharSe extends Modificador {
-    constructor(valor: string, quantificador: string) {
+    valoresAceitos: { [valorFoles: string]: string } = {
+        "inicio": "start",
+        "início": "start",
+        "fim": "end",
+        "flex-inicio": "flex-start",
+        "flex-início": "flex-start",
+        "flex-fim": "flex-end",
+        "centro": "center",
+        "normal": "normal",
+        "linha-de-base": "alternate",
+        "inicio-linha-de-base": "first baseline",
+        "início-linha-de-base": "first baseline",
+        "fim-linha-de-base": "last baseline",
+        "espaco-entre": "space-between",
+        "espaço-entre": "space-between",
+        "espaco-ao-redor": "space-around",
+        "espaço-ao-redor": "space-around",
+        "espaco-uniforme": "space-evenly",
+        "espaço-uniforme": "space-evenly",
+        "esticar": "stretch",
+        "seguro": "safe",
+        "inseguro": "unsafe",
+        "auto-inicio": "self-start",
+        "auto-início": "self-start",
+        "auto-fim": "self-end",
+        "auto": "auto",
+    }
+
+    constructor(valor: string, quantificador?: string) {
         super("alinhar-se", "align-self");
+
+        if (!(valor in this.valoresAceitos)) {
+            throw new Error(`Valor ${valor} inválido para 'alinhar-se'. Valores aceitos: ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += `, ${atual}`)}.`);
+        }
+
         this.valor = valor;
-        this.quantificador = quantificador;
+
+        // Não recebe quantificador
+        // this.quantificador = quantificador;
     }
 }
