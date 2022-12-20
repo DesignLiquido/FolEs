3883fc415ac99ed2a179c652d9939a8d9e4d87a3
VitBrandao <vitorsbsilva@gmail.com>
2022-12-20 10:13:54 -0300

    Modificadores Fundo de Posição: Adicionando valores

--- fontes/modificadores/posicao-horizontal-fundo.ts
+++ fontes/modificadores/posicao-horizontal-fundo.ts
@@ -1,12 +1,40 @@
 import { Modificador } from "./superclasse/modificador";
 
 export class PosicaoHorizontalFundo extends Modificador {
-    constructor(valor: string, quantificador: string) {
+    valoresAceitos: { [valorFoles: string]: string } = {
+        "superior": "top",
+        "inferior": "bottom",
+        "esquerda": "left",
+        "direita": "right",
+        "centro": "center",
+    }
+
+    constructor(valor: string, quantificador?: string) {
         super(
-            ["posicao-horizontal-fundo", "posição-horizontal-fundo"], 
+            ["posicao-horizontal-fundo", "posição-horizontal-fundo"],
             "background-position-x"
         );
+
+        // Aceita valores listados e valores numéricos
+        if (
+            Number.isNaN(parseInt(valor)) &&
+            !(valor in this.valoresAceitos)
+        ) {
+            throw new Error(`Propriedade 'posição-horizontal-fundo' com valor ${valor} inválido. Valor deve ser um número ou um dos valores aceitos: ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += `, ${atual}`)}.`)
+        }
+
         this.valor = valor;
-        this.quantificador = quantificador;
+        
+        if (Number(parseInt(valor))){
+            const quantificadoresAceitos = [
+                'px', '%', 'rem', 'vmin', 'vmax'
+            ];
+    
+            if (!quantificadoresAceitos.includes(quantificador)) {
+                throw new Error(`Propriedade 'posição-horizontal-fundo' com quantificador ${quantificador} inválido. Valores aceitos: ${quantificadoresAceitos} .`);
+            }
+    
+            this.quantificador = quantificador;
+        }
     }
 }
