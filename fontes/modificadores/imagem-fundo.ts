98152915e2eb3bed1207f8f7c25b8ebf09c26a33
VitBrandao <vitorsbsilva@gmail.com>
2022-12-20 10:42:11 -0300

    Adicionando valores: repetir, mesclar e imagem-fundo

--- fontes/modificadores/imagem-fundo.ts
+++ fontes/modificadores/imagem-fundo.ts
@@ -1,9 +1,21 @@
 import { Modificador } from "./superclasse/modificador";
 
 export class ImagemFundo extends Modificador {
-    constructor(valor: string, quantificador: string) {
+    valoresAceitos: { [valorFoles: string]: string } = {
+        "nenhuma": "none",
+    }
+
+    constructor(valor: string, quantificador?: string) {
         super("imagem-fundo", "background-image");
+
+        // Valor deve ser um link URL ou 'nenhuma' (none)
+        if(!(valor.includes('url(')) && !(valor in this.valoresAceitos)) {
+            throw new Error(`Propriedade 'imagem-fundo' com valor ${valor} inválido. O valor deve ser uma URL ou um dos valores aceitos: ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += `, ${atual}`)}.`)
+        }
+
         this.valor = valor;
-        this.quantificador = quantificador;
+
+        // Não recebe quantificador
+        // this.quantificador = quantificador;
     }
 }
