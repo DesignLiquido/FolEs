fc37d3bfe26a0a49c8347b1c2a363673b58f8679
VitBrandao <vitorsbsilva@gmail.com>
2022-12-20 14:53:20 -0300

    Adicionando valores: cor-fundo

--- fontes/modificadores/cor-fundo.ts
+++ fontes/modificadores/cor-fundo.ts
@@ -1,9 +1,25 @@
+import { ListaDeCores } from "./atributos/cores";
 import { Modificador } from "./superclasse/modificador";
 
 export class CorFundo extends Modificador {
-    constructor(valor: string, quantificador: string) {
+    constructor(valor: string, quantificador?: string) {
         super("cor-fundo", "background-color");
         this.valor = valor;
-        this.quantificador = quantificador;
+
+        // Lista de cores como valor
+        if (!(valor in ListaDeCores)) {
+            throw new Error(`Propriedade 'cor-fundo' com valor ${valor} inválido. Valores aceitos: ${Object.keys(ListaDeCores).reduce((final, atual) => final += `, ${atual}`)}.`)
+        }
+
+        // Há ainda os casos de:
+        // rgb / rgba - ex.: rgb(255, 255, 128)
+        // hsl / hsla - ex.: hsl(50, 33%, 25%)
+
+
+        // Não recebe quantificador
+        // this.quantificador = quantificador;
     }
 }
+
+const a = new CorFundo("azl");
+console.log(a);
