868351821fb77b91a0cb2fdb87d86f15b8ba557b
VitBrandao <vitorsbsilva@gmail.com>
2022-12-21 14:12:54 -0300

    Refatora seletores Alinhar de acordo com lista de posições

--- fontes/modificadores/alinhar-itens.ts
+++ fontes/modificadores/alinhar-itens.ts
@@ -1,16 +1,8 @@
+import { ListaDePosições } from "./atributos/posição";
 import { Modificador } from "./superclasse/modificador";
 
 export class AlinharItens extends Modificador {
     valoresAceitos: { [valorFoles: string]: string } = {
-        "inicio": "start",
-        "início": "start",
-        "fim": "end",
-        "flex-inicio": "flex-start",
-        "flex-início": "flex-start",
-        "flex-fim": "flex-end",
-        "centro": "center",
-        "normal": "normal",
-        "linha-de-base": "alternate",
         "inicio-linha-de-base": "first baseline",
         "início-linha-de-base": "first baseline",
         "fim-linha-de-base": "last baseline",
@@ -20,7 +12,6 @@ export class AlinharItens extends Modificador {
         "espaço-ao-redor": "space-around",
         "espaco-uniforme": "space-evenly",
         "espaço-uniforme": "space-evenly",
-        "esticar": "stretch",
         "seguro": "safe",
         "inseguro": "unsafe",
     }
@@ -28,8 +19,11 @@ export class AlinharItens extends Modificador {
     constructor(valor: string, quantificador?: string) {
         super("alinhar-itens", "align-items");
 
-        if (!(valor in this.valoresAceitos)) {
-            throw new Error(`Valor ${valor} inválido para 'alinhar-itens'. Valores aceitos: ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += `, ${atual}`)}.`);
+        // Pode receber valores próprios ou valores da lista de posições
+        if (!(valor in this.valoresAceitos) && !(valor in ListaDePosições)) {
+            throw new Error(`Valor ${valor} inválido para 'alinhar-itens'. Valores aceitos:
+            ${Object.keys(ListaDePosições).reduce((final, atual) => final += `, ${atual}`)}, 
+            ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += `, ${atual}`)}.`);
         }
 
         this.valor = valor;
--- fontes/modificadores/alinhar-se.ts
+++ fontes/modificadores/alinhar-se.ts
@@ -1,16 +1,8 @@
+import { ListaDePosições } from "./atributos/posição";
 import { Modificador } from "./superclasse/modificador";
 
 export class AlinharSe extends Modificador {
     valoresAceitos: { [valorFoles: string]: string } = {
-        "inicio": "start",
-        "início": "start",
-        "fim": "end",
-        "flex-inicio": "flex-start",
-        "flex-início": "flex-start",
-        "flex-fim": "flex-end",
-        "centro": "center",
-        "normal": "normal",
-        "linha-de-base": "alternate",
         "inicio-linha-de-base": "first baseline",
         "início-linha-de-base": "first baseline",
         "fim-linha-de-base": "last baseline",
@@ -20,7 +12,6 @@ export class AlinharSe extends Modificador {
         "espaço-ao-redor": "space-around",
         "espaco-uniforme": "space-evenly",
         "espaço-uniforme": "space-evenly",
-        "esticar": "stretch",
         "seguro": "safe",
         "inseguro": "unsafe",
         "auto-inicio": "self-start",
@@ -32,8 +23,11 @@ export class AlinharSe extends Modificador {
     constructor(valor: string, quantificador?: string) {
         super("alinhar-se", "align-self");
 
-        if (!(valor in this.valoresAceitos)) {
-            throw new Error(`Valor ${valor} inválido para 'alinhar-se'. Valores aceitos: ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += `, ${atual}`)}.`);
+        // Pode receber valores próprios ou valores da lista de posições
+        if (!(valor in this.valoresAceitos) && !(valor in ListaDePosições)) {
+            throw new Error(`Valor ${valor} inválido para 'alinhar-se'. Valores aceitos:
+            ${Object.keys(ListaDePosições).reduce((final, atual) => final += `, ${atual}`)}, 
+            ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += `, ${atual}`)}.`);
         }
 
         this.valor = valor;
