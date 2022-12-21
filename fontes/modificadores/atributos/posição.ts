99e8828c94e4df6af5368ff94b94a1ff0fd81719
VitBrandao <vitorsbsilva@gmail.com>
2022-12-21 14:12:37 -0300

    Cria lista de Posições

---
+++ fontes/modificadores/atributos/posição.ts
@@ -0,0 +1,16 @@
+export const ListaDePosições: { [valorFolEs: string]: string } = {
+    "inicio": "start",
+    "início": "start",
+    "fim": "end",
+    "normal": "normal",
+    "esticar": "stretch",
+    "centro": "center",
+    "linha-de-base": "baseline",
+    "flex-inicio": "flex-start",
+    "flex-início": "flex-start",
+    "flex-fim": "flex-end",
+    "superior": "top",
+    "inferior": "bottom",
+    "direita": "right",
+    "esquerda": "left",
+}
