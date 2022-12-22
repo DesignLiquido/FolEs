8db707d417ae6cbdc357b26af36fa0977165b7e1
VitBrandao <vitorsbsilva@gmail.com>
2022-12-22 11:09:36 -0300

    Cria lista de valores Globais

---
+++ fontes/modificadores/atributos/globais.ts
@@ -0,0 +1,7 @@
+export const ListaDeValoresGlobais: { [valorFolEs: string]: string } = {
+    "herdar": "inherit",
+    "inicial": "initial",
+    "reverter": "revert",
+    "reverter-camada": "revert-layer",
+    "desarmar": "unset",
+}
