f52caf718c1a23332cd25ed3a6da692a19fd87eb
VitBrandao <vitorsbsilva@gmail.com>
2023-01-03 09:47:28 -0300

    Adiciona valores aos modificadores largura-borda-em-bloco

--- fontes/modificadores/largura-fim-borda-em-bloco.ts
+++ fontes/modificadores/largura-fim-borda-em-bloco.ts
@@ -1,9 +1,41 @@
+import { ListaDeValoresGlobais } from "./atributos/globais";
+import { ListaDeQuantificadores } from "./atributos/quantificadores";
 import { Modificador } from "./superclasse/modificador";
 
 export class LarguraFimBordaEmBloco extends Modificador {
-    constructor(valor: string, quantificador: string) {
+    valoresAceitos: { [valorFoles: string]: string } = {
+        "fina": "thin",
+        "medio": "medium",
+        "médio": "medium",
+        "grossa": "thick",
+    }
+
+    constructor(valor: string, quantificador?: string) {
         super("largura-fim-borda-em-bloco", "border-block-end-width");
+
+        // Pode receber valores próprios ou número-quantificador
+        if (Number.isNaN(parseInt(valor)) &&
+            !(valor in this.valoresAceitos) &&
+            !(valor in ListaDeValoresGlobais)
+        ) {
+            throw new Error(`Propriedade 'largura-fim-borda-em-bloco' com valor ${valor} inválido. O valor deve ser numérico ou um dos valores:
+            ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += `, ${atual}`)},
+            ${Object.keys(ListaDeValoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`);
+        }
+
         this.valor = valor;
-        this.quantificador = quantificador;
+
+        if (Number(parseInt(valor))){
+            if (
+                !(quantificador in ListaDeQuantificadores) ||
+                quantificador === undefined
+            ) {
+                throw new Error(
+                `Propriedade 'largura-fim-borda-em-bloco' com quantificador inválido. Valores aceitos:
+                ${Object.keys(ListaDeQuantificadores).reduce((final, atual) => final += `, ${atual}`)}.`);
+            }
+    
+            this.quantificador = quantificador;
+        }
     }
 }
