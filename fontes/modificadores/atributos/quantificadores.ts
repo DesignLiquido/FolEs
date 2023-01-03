ddc5d1c54fc66e8c134e64e88ce6d3c6d352e943
VitBrandao <vitorsbsilva@gmail.com>
2023-01-03 09:31:35 -0300

    Cria lista de quantificadores

---
+++ fontes/modificadores/atributos/quantificadores.ts
@@ -0,0 +1,73 @@
+export const ListaDeQuantificadores: { [valorFolEs: string]: string } = {
+    "px": "px",
+    "cm": "cm",
+    "mm": "mm",
+    "Q": "Q",
+    "in": "in",
+    "pc": "pc",
+    "pt": "pt",
+    "%": "%",
+    "vh": "vh",
+    "vw": "vw",
+    "vmax": "vmax",
+    "vmin": "vmin",
+    "vb": "vb",
+    "vi": "vi",
+    "cqw": "cqw",
+    "cqh": "cqh",
+    "cqi": "cqi",
+    "cqb": "cqb",
+    "cqmin": "cqmin",
+    "cqmax": "cqmax",
+    "cap": "cap",
+    "ch": "ch",
+    "em": "em",
+    "ex": "ex",
+    "ic": "ic",
+    "lh": "lh",
+    "rem": "rem",
+    "rlh": "rlh",
+}
+
+export const ListaDeValoresAbsolutos: { [valorFolEs: string]: string } = {
+    "px": "px",
+    "cm": "cm",
+    "mm": "mm",
+    "Q": "Q",
+    "in": "in",
+    "pc": "pc",
+    "pt": "pt",
+}
+
+export const ListaDeValorPercentual: { [valorFolEs: string]: string } = {
+    "%": "%",
+}
+
+export const ListaDeValoresVisualização: { [valorFolEs: string]: string } = {
+    "vh": "vh",
+    "vw": "vw",
+    "vmax": "vmax",
+    "vmin": "vmin",
+    "vb": "vb",
+    "vi": "vi",
+}
+
+export const ListaDeValoresConteiner: { [valorFolEs: string]: string } = {
+    "cqw": "cqw",
+    "cqh": "cqh",
+    "cqi": "cqi",
+    "cqb": "cqb",
+    "cqmin": "cqmin",
+    "cqmax": "cqmax",
+}
+
+export const ListaDeValoresFonte: { [valorFolEs: string]: string } = {
+    "cap": "cap",
+    "ch": "ch",
+    "em": "em",
+    "ex": "ex",
+    "ic": "ic",
+    "lh": "lh",
+    "rem": "rem",
+    "rlh": "rlh",
+}
