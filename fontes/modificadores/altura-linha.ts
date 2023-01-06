011906cf495f58ebb788cf7d135460f6f9f3175e
VitBrandao <vitorsbsilva@gmail.com>
2023-01-06 11:03:15 -0300

    Adiciona valores aos modificadores Inserção, Margem e +3

--- fontes/modificadores/altura-linha.ts
+++ fontes/modificadores/altura-linha.ts
@@ -1,9 +1,38 @@
+import { ListaDeValoresGlobais } from "./atributos/globais";
+import { ListaDeQuantificadores } from "./atributos/quantificadores";
 import { Modificador } from "./superclasse/modificador";
 
 export class AlturaLinha extends Modificador {
-    constructor(valor: string, quantificador: string) {
+    valoresAceitos: { [valorFoles: string]: string } = {
+        "normal": "normal",
+    }
+
+    constructor(valor: string, quantificador?: string) {
         super("altura-linha", "line-height");
+
+        if (Number.isNaN(parseInt(valor)) &&
+            !(valor in this.valoresAceitos) &&
+            !(valor in ListaDeValoresGlobais)
+        ) {
+            throw new Error(
+                `Propriedade 'espaçamento-letras' com valor ${valor} inválido. O valor deve ser numérico ou um dos valores:
+                ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += `, ${atual}`)},
+                ${Object.keys(ListaDeValoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`);
+        }
+
         this.valor = valor;
-        this.quantificador = quantificador;
+
+        // Lógica diferente dos demais pois o modificador pode receber valores numéricos s/ quantificador
+        if (quantificador !== undefined) {
+            if (
+                !(quantificador in ListaDeQuantificadores)
+            ) {
+                throw new Error(
+                    `Propriedade 'espaçamento-letras' com quantificador inválido. Valores aceitos:
+                    ${Object.keys(ListaDeQuantificadores).reduce((final, atual) => final += `, ${atual}`)}.`);
+            }
+
+            this.quantificador = quantificador;
+        }
     }
 }
--- fontes/modificadores/insercao.ts
+++ fontes/modificadores/insercao.ts
@@ -1,9 +1,38 @@
+import { ListaDeValoresGlobais } from "./atributos/globais";
+import { ListaDeQuantificadores } from "./atributos/quantificadores";
 import { Modificador } from "./superclasse/modificador";
 
 export class Insercao extends Modificador {
-    constructor(valor: string, quantificador: string) {
+    valoresAceitos: { [valorFoles: string]: string } = {
+        "auto": "auto",
+    }
+
+    constructor(valor: string, quantificador?: string) {
         super(["insercao", "inserção"], "inset");
+
+        if (Number.isNaN(parseInt(valor)) &&
+            !(valor in this.valoresAceitos) &&
+            !(valor in ListaDeValoresGlobais)
+        ) {
+            throw new Error(
+                `Propriedade 'inserção' com valor ${valor} inválido. O valor deve ser numérico ou um dos valores:
+            ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += `, ${atual}`)},
+            ${Object.keys(ListaDeValoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`);
+        }
+
         this.valor = valor;
-        this.quantificador = quantificador;
+
+        if (Number(parseInt(valor))) {
+            if (
+                !(quantificador in ListaDeQuantificadores) ||
+                quantificador === undefined
+            ) {
+                throw new Error(
+                    `Propriedade 'inserção' com quantificador inválido. Valores aceitos:
+                ${Object.keys(ListaDeQuantificadores).reduce((final, atual) => final += `, ${atual}`)}.`);
+            }
+
+            this.quantificador = quantificador;
+        }
     }
 }
