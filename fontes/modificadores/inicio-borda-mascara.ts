eba5dd455245815e2b7cfda8af1428e05bb86cf0
VitBrandao <vitorsbsilva@gmail.com>
2023-01-06 11:22:45 -0300

    Adiciona valores aos modificadores Máscara

--- fontes/modificadores/inicio-borda-mascara.ts
+++ fontes/modificadores/inicio-borda-mascara.ts
@@ -1,12 +1,38 @@
+import { ListaDeValoresGlobais } from "./atributos/globais";
+import { ListaDeComprimento } from "./atributos/quantificadores";
 import { Modificador } from "./superclasse/modificador";
 
 export class InicioBordaMascara extends Modificador {
-    constructor(valor: string, quantificador: string) {
+    constructor(valor: string, quantificador?: string) {
         super(
-            ["inicio-borda-mascara", "início-borda-máscara"], 
+            ["inicio-borda-mascara", "início-borda-máscara"],
             "mask-border-outset"
         );
+
+        // OBS.: Pode receber de 1 a 4 valores
+        // A lógica abaixo cobre somente o recebimento de um único valor
+        if (Number.isNaN(parseInt(valor)) &&
+            !(valor in ListaDeValoresGlobais)
+        ) {
+            throw new Error(
+                `Propriedade 'início-borda-máscara' com valor ${valor} inválido. O valor deve ser numérico ou um dos valores:
+            ${Object.keys(ListaDeValoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`);
+        }
+
         this.valor = valor;
-        this.quantificador = quantificador;
+
+        // Não aceita valores percentuais, somente de comprimento
+        if (Number(parseInt(valor))) {
+            if (
+                !(quantificador in ListaDeComprimento) ||
+                quantificador === undefined
+            ) {
+                throw new Error(
+                    `Propriedade 'início-borda-máscara' com quantificador inválido. Valores aceitos:
+                ${Object.keys(ListaDeComprimento).reduce((final, atual) => final += `, ${atual}`)}.`);
+            }
+
+            this.quantificador = quantificador;
+        }
     }
 }
