20c9fa45dca51dd4d549d57257173c73f1efb9be
VitBrandao <vitorsbsilva@gmail.com>
2023-01-31 14:43:15 -0300

    Conserta testes de Palavras Reservadas

--- testes/modificadores/palavras-reservadas.test.ts
+++ testes/modificadores/palavras-reservadas.test.ts
@@ -17,11 +17,83 @@ describe('Testando Seletores que recebem PALAVRAS RESERVADAS como atributo', () 
             tradutor = new Tradutor();
         });
 
-        it('Trivial', () => {
-            expect(1).toBe(1);
+        it('Testando seletores de Palavras Reservadas com valores globais', () => {
+            for (let index = 0; index < Object.keys(PalavrasReservadas).length; index += 1) {
+                const seletor = new SeletorModificador(PalavrasReservadas[index], 'reverter', null);
+
+                // Lexador
+                const resultadoLexador = lexador.mapear([
+                    "corpo {",
+                    `${PalavrasReservadas[index]}: ${seletor['valor']};`,
+                    "}"
+                ]);
+
+                // O modificador deve aceitar o valor global
+                expect(seletor['valor']).toEqual('reverter');
+
+                // O Lexador deve montar um objeto de comprimento 7 sem retornar nenhum erro
+                expect(resultadoLexador.simbolos).toHaveLength(7);
+                expect(resultadoLexador.erros).toHaveLength(0);
+
+                // O Lexador não deve encontrar qualquer número ou quantificador na operação
+                expect(resultadoLexador.simbolos).not.toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({ tipo: tiposDeSimbolos.QUANTIFICADOR }),
+                        expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
+                    ])
+                );
+
+                // // Avaliador Sintático
+                // const resultadoAvaliadorSintatico = avaliador.analisar(resultadoLexador.simbolos);
+
+                // expect(resultadoAvaliadorSintatico[0].modificadores[0].nomeFoles).toStrictEqual(
+                //     seletor['nomeFoles']
+                // );
+                // expect(resultadoAvaliadorSintatico[0].modificadores[0].propriedadeCss).toStrictEqual(
+                //     seletor['propriedadeCss']
+                // );
+
+
+                // // Tradutor
+                // const resultadoTradutor = tradutor.traduzir(resultadoAvaliadorSintatico);
+
+                // expect(resultadoTradutor).toContain(seletor['propriedadeCss']);
+                // expect(resultadoTradutor).toContain('inherit');
+            }
         });
 
-        // ARQUIVO DE TESTES CRIADO PARA PENSARMOS EM UMA LÓGICA DE COMO
-        // TESTAR OS SELETORES QUE RECEBEM PALAVRAS ESPECÍFICAS COMO ATRIBUTO
+        it('Testando seletores de Palavras Reservadas com os referidos valores aceitos', () => {
+            for (let index = 0; index < Object.keys(PalavrasReservadas).length; index += 1) {
+                let seletor = new SeletorModificador(PalavrasReservadas[index], 'reverter', null);
+
+                // Se há uma lista de valores aceitos, é atribuído o primeiro valor ao seletor.
+                // Se não há, a condicional abaixo não é executada e o seletor segue sendo o da linha acima.
+                if(seletor['valoresAceitos'] !== undefined) {
+                    const valor = Object.keys(seletor['valoresAceitos']);
+                    seletor = new SeletorModificador(
+                        PalavrasReservadas[index], valor[0], null
+                    );
+                };
+
+                // Lexador
+                const resultadoLexador = lexador.mapear([
+                    "corpo {",
+                    `${PalavrasReservadas[index]}: ${seletor['valor']};`,
+                    "}"
+                ]);
+
+                // O Lexador deve montar um objeto de comprimento 7 sem retornar nenhum erro
+                expect(resultadoLexador.simbolos).toHaveLength(7);
+                expect(resultadoLexador.erros).toHaveLength(0);
+
+                // O Lexador não deve encontrar qualquer número ou quantificador na operação
+                expect(resultadoLexador.simbolos).not.toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({ tipo: tiposDeSimbolos.QUANTIFICADOR }),
+                        expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
+                    ])
+                );
+            }
+        });
     });
 });
