4615fb2afccb1d618b60718731d5f0aaa27989ee
VitBrandao <vitorsbsilva@gmail.com>
2023-02-02 16:34:21 -0300

    Adiciona validações ao modificador-exemplo

--- fontes/modificadores/cor-destaque.ts
+++ fontes/modificadores/cor-destaque.ts
@@ -1,9 +1,28 @@
+import { ListaDeCores } from "./atributos/cores";
+import { ListaDeValoresGlobais } from "./atributos/globais";
 import { Modificador } from "./superclasse/modificador";
 
 export class CorDestaque extends Modificador {
-    constructor(valor: string, quantificador: string) {
+    constructor(valor: string, quantificador?: string) {
         super("cor-destaque", "accent-color");
+ 
+        // O valor é recebido como objeto, o que impossibilita de utilizar a função includes().
+        // A constante abaixo é criada para não ocorrer esse problema.
+        const valorString = valor.toString();
+        
+        if (!(valor in ListaDeCores) &&
+            !(valor in ListaDeValoresGlobais) &&
+            !(valorString.includes('rgb')) &&
+            !(valorString.includes('rgba')) &&
+            !(valorString.includes('hsl'))
+        ) {
+            throw new Error(`Propriedade 'cor-destaque' com valor ${valor} inválido. Valores aceitos:
+            rgb, rgba, hsl,  
+            ${Object.keys(ListaDeCores).reduce((final, atual) => final += `, ${atual}`)},
+            ${Object.keys(ListaDeValoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`);
+        }
+
         this.valor = valor;
-        this.quantificador = quantificador;
+        // this.quantificador = quantificador;
     }
 }
