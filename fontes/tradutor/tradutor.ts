b7298bbce335edd505154d78393ba04dabe096cd
Vitor Brandão <82619724+VitBrandao@users.noreply.github.com>
2023-02-23 12:45:37 -0300

    Adaptando Tradutor para o caso de valores string (#53)

--- fontes/tradutor/tradutor.ts
+++ fontes/tradutor/tradutor.ts
@@ -1,10 +1,35 @@
 import { Declaracao } from "../declaracoes";
 import { Modificador } from "../modificadores";
+import { valoresGerais } from "../modificadores/atributos/gerais";
 import estruturasHtml from "./estruturas-html";
 
 export class Tradutor {
     traduzirModificador(modificador: Modificador): string {
-        return `\t${modificador.propriedadeCss}: ${modificador.valor}${modificador.quantificador || ''};\n`;
+        // Caso 1: Número-Quantificador ou somente Número. 
+        if (Number(modificador.valor)){
+            return `\t${modificador.propriedadeCss}: ${modificador.valor}${modificador.quantificador || ''};\n`;
+        } else {
+            
+            // Caso 2: Tradução do valor contida no objeto 'valoresAceitos'.
+            if (modificador['valoresAceitos'] !== undefined && 
+                modificador['valoresAceitos'].hasOwnProperty(modificador.valor)
+            ){
+                const objetoValores = modificador['valoresAceitos'];
+                const valorTraduzido = objetoValores[modificador.valor];
+                return `\t${modificador.propriedadeCss}: ${valorTraduzido};\n`;
+            }
+            
+            // Caso 3: Valor é RGB, RGBA, HSL, HSLA ou HEX, ou seja, um valor do tipo objeto. 
+            if (typeof(modificador.valor) === 'object') {
+                return `\t${modificador.propriedadeCss}: ${modificador.valor}${modificador.quantificador || ''};\n`;
+            } 
+            
+            // Caso 4: É um valor genérico, cuja tradução está na lista 'valoresGerais'.
+            else {
+                const valorTraduzido = valoresGerais[modificador.valor];
+                return `\t${modificador.propriedadeCss}: ${valorTraduzido};\n`; 
+            }
+        }
     }
 
     traduzir(declaracoes: Declaracao[]) {
