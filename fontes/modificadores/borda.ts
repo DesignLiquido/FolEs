1b6d8c94ba73b94708dda91a09d47e51ac812003
VitBrandao <vitorsbsilva@gmail.com>
2023-03-03 11:40:19 -0300

    Adiciona validação de valores aos seletores de Atribuição Abreviada

--- fontes/modificadores/borda.ts
+++ fontes/modificadores/borda.ts
@@ -1,9 +1,55 @@
+import { cores } from "./atributos/cores";
+import { estilos } from "./atributos/estilo";
+import { valoresGlobais } from "./atributos/globais";
 import { Modificador } from "./superclasse/modificador";
+import { unidadesMedida } from "./atributos/quantificadores";
 
 export class Borda extends Modificador {
-    constructor(valor: string, quantificador: string) {
+    // Seletor de Atribuição Abreviada (Shorthand).
+    // Pode receber de 1 a 3 valores.
+    valoresAceitos: { [valorFoles: string]: string } = {
+        "fina": "thin",
+        "media": "medium",
+        "média": "medium",
+        "espessa": "thick",
+    }
+
+    constructor(valor: string, quantificador?: string) {
         super("borda", "border");
+
+        // O valor é recebido como objeto, o que impossibilita de utilizar a função includes().
+        // A constante abaixo é criada para ser possível fazer as validações seguintes.
+        const valorString = valor.toString();
+
+        const validaçõesCor = !(valorString.includes('rgb')) && !(valorString.includes('rgba')) &&
+            !(valorString.includes('hsl')) && !(valorString.includes('hsla'));
+
+        const validaçõesHEX = !(valorString.startsWith('#') && valorString.length <= 7);
+
+        if (!(valor in this.valoresAceitos) &&
+            Number.isNaN(parseInt(valor)) &&
+            validaçõesCor &&
+            validaçõesHEX &&
+            !(valor in estilos) &&
+            !(valor in cores) &&
+            !(valor in valoresGlobais)) {
+            throw new Error(`Propriedade 'borda' com valor ${valor} inválido. Valores aceitos: 
+            número-quantificador, 
+            ${Object.keys(this.valoresAceitos).reduce((final, atual) => final += `, ${atual}`)},
+            ${Object.keys(estilos).reduce((final, atual) => final += `, ${atual}`)},
+            ${Object.keys(cores).reduce((final, atual) => final += `, ${atual}`)},
+            ${Object.keys(valoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`);
+        }
+
         this.valor = valor;
-        this.quantificador = quantificador;
+
+        if (Number(parseInt(valor))) {
+            if (!(quantificador in unidadesMedida) || quantificador === undefined) {
+                throw new Error(`Propriedade 'borda' com quantificador inválido. Valores aceitos:
+                ${Object.keys(unidadesMedida).reduce((final, atual) => final += `, ${atual}`)}.`);
+            }
+
+            this.quantificador = quantificador;
+        }
     }
 }
--- fontes/modificadores/estilo-borda.ts
+++ fontes/modificadores/estilo-borda.ts
@@ -1,9 +1,24 @@
+import { estilos } from "./atributos/estilo";
+import { valoresGlobais } from "./atributos/globais";
 import { Modificador } from "./superclasse/modificador";
 
 export class EstiloBorda extends Modificador {
+    // Seletor de Atribuição Abreviada (Shorthand).
+    // Pode receber de 1 a 4 valores.
+
     constructor(valor: string, quantificador: string) {
         super("estilo-borda", "border-style");
+
+        if (!(valor in estilos) &&
+            !(valor in valoresGlobais)) {
+            throw new Error(`Propriedade 'estilo-borda' com valor ${valor} inválido. Valores aceitos: 
+        ${Object.keys(estilos).reduce((final, atual) => final += `, ${atual}`)},
+        ${Object.keys(valoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`);
+        }
+
         this.valor = valor;
-        this.quantificador = quantificador;
+
+        // Não recebe quantificador
+        // this.quantificador = quantificador;
     }
 }
