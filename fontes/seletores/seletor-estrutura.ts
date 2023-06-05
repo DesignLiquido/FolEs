8b7269c31b254d6b8b42311c5c4604836383833d
VitBrandao <vitorsbsilva@gmail.com>
2023-06-05 13:14:46 -0300

    Otimizando método paraTexto() na classe SeletorEstrutura

--- fontes/seletores/seletor-estrutura.ts
+++ fontes/seletores/seletor-estrutura.ts
@@ -11,7 +11,15 @@ export class SeletorEstrutura extends Seletor {
     }
 
     paraTexto() {
-        let resultado = this.estrutura.constructor.name.toLowerCase();
+        const nomeTag = this.estrutura.constructor.name;
+        
+        // Convertendo a primeira letra em minúscula
+        const nomeTagFormatado = nomeTag.charAt(0).toLowerCase() + nomeTag.slice(1);
+
+        // Convertendo as letras maiúsculas restantes para minúsculas antecedidas por hífen.
+        // Desta forma, as classes em camelCase (ex.: areaTexto) viram kebab-case (area-texto)
+        let resultado = nomeTagFormatado.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`);
+
         if (this.pseudoclasse !== undefined && this.pseudoclasse !== null) {
             resultado += `:${this.pseudoclasse.pseudoclasseCss}`;
         }
