fe180f8d63d6300089f80f208b5b86dd1e8db78a
VitBrandao <vitorsbsilva@gmail.com>
2023-06-07 10:59:22 -0300

    Criando função de validação para valores numéricos

---
+++ fontes/modificadores/validacoes/numerica.ts
@@ -0,0 +1,22 @@
+import { valoresGlobais } from "../atributos/globais";
+
+export function validarValorNumerico(nomePropriedade: string, valor: any, valoresAceitos?: { [valorFoles: string]: string }) {
+    if (valoresAceitos !== undefined) {
+        if (Number.isNaN(parseInt(valor)) &&
+            !(valor in valoresAceitos) &&
+            !(valor in valoresGlobais)
+        ) {
+            throw new Error(`Propriedade '${nomePropriedade}' com valor ${valor} inválido. Valores aceitos: 
+            ${Object.keys(valoresAceitos).reduce((final, atual) => final += `, ${atual}`)},
+            ${Object.keys(valoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`)
+        }
+    } else {
+        if (Number.isNaN(parseInt(valor)) &&
+            !(valor in valoresGlobais)
+        ) {
+            throw new Error(`Propriedade '${nomePropriedade}' com valor ${valor} inválido. Valores aceitos: 
+            ${Object.keys(valoresAceitos).reduce((final, atual) => final += `, ${atual}`)},
+            ${Object.keys(valoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`)
+        }
+    }
+}
