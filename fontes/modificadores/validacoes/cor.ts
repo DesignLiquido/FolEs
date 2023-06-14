65c86c4e69cb72f5a2dc6b0ac27cba3457c21bac
VitBrandao <vitorsbsilva@gmail.com>
2023-06-14 15:35:15 -0300

    Otimiza funções universais de validação de Cor e Numérica

--- fontes/modificadores/validacoes/cor.ts
+++ fontes/modificadores/validacoes/cor.ts
@@ -5,7 +5,7 @@ export function validarValorCor(
     nomePropriedade: string,
     valor: any,
     valoresAceitos?: { [valorFoles: string]: string },
-    valoresExtra?: any) 
+    valoresExtra?: { [valorFoles: string]: string }) 
 {
     // O valor é recebido como objeto, o que impossibilita de utilizar a função includes().
     // A constante abaixo é criada para não ocorrer esse problema.
@@ -55,6 +55,23 @@ export function validarValorCor(
             rgb(), rgba(), hsl(), hsla(), #hex, 
             ${Object.keys(cores).reduce((final, atual) => final += `, ${atual}`)},    
             ${Object.keys(valoresAceitos).reduce((final, atual) => final += `, ${atual}`)},
+            ${Object.keys(valoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`)
+        }
+    }
+
+    if (valoresAceitos !== undefined && valoresExtra !== undefined) {
+        if (!(valor in cores) &&
+            validaçõesCor &&
+            validaçõesHEX &&
+            !(valor in valoresAceitos) &&
+            !(valor in valoresExtra) &&
+            !(valor in valoresGlobais)
+        ) {
+            throw new Error(`Propriedade '${nomePropriedade}' com valor ${valor} inválido. Valores aceitos:
+            rgb(), rgba(), hsl(), hsla(), #hex, 
+            ${Object.keys(cores).reduce((final, atual) => final += `, ${atual}`)},    
+            ${Object.keys(valoresAceitos).reduce((final, atual) => final += `, ${atual}`)},
+            ${Object.keys(valoresExtra).reduce((final, atual) => final += `, ${atual}`)},
             ${Object.keys(valoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`)
         }
     } 
--- fontes/modificadores/validacoes/numerica.ts
+++ fontes/modificadores/validacoes/numerica.ts
@@ -1,11 +1,22 @@
 import { valoresGlobais } from "../atributos/globais";
 
 export function validarValorNumerico(
-    nomePropriedade: string, 
-    valor: any, 
-    valoresAceitos?: { [valorFoles: string]: string }) 
+    nomePropriedade: string,
+    valor: any,
+    valoresAceitos?: { [valorFoles: string]: string },
+    valoresExtra?: { [valorFoles: string]: string }) 
 {
-    if (valoresAceitos !== undefined) {
+    if (valoresAceitos === undefined && valoresExtra === undefined) {
+        if (Number.isNaN(parseInt(valor)) &&
+            !(valor in valoresGlobais)
+        ) {
+            throw new Error(`Propriedade '${nomePropriedade}' com valor ${valor} inválido. Valores aceitos:
+            número-quantificador,
+            ${Object.keys(valoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`)
+        }
+    }
+
+    if (valoresAceitos !== undefined && valoresExtra === undefined) {
         if (Number.isNaN(parseInt(valor)) &&
             !(valor in valoresAceitos) &&
             !(valor in valoresGlobais)
@@ -15,12 +26,30 @@ export function validarValorNumerico(
             ${Object.keys(valoresAceitos).reduce((final, atual) => final += `, ${atual}`)},
             ${Object.keys(valoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`)
         }
-    } else {
+    }
+
+    if (valoresAceitos === undefined && valoresExtra !== undefined) {
         if (Number.isNaN(parseInt(valor)) &&
+            !(valor in valoresExtra) &&
             !(valor in valoresGlobais)
         ) {
             throw new Error(`Propriedade '${nomePropriedade}' com valor ${valor} inválido. Valores aceitos:
             número-quantificador,
+            ${Object.keys(valoresExtra).reduce((final, atual) => final += `, ${atual}`)},
+            ${Object.keys(valoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`)
+        }
+    }
+
+    if (valoresAceitos !== undefined && valoresExtra !== undefined) {
+        if (Number.isNaN(parseInt(valor)) &&
+            !(valor in valoresAceitos) &&
+            !(valor in valoresExtra) &&
+            !(valor in valoresGlobais)
+        ) {
+            throw new Error(`Propriedade '${nomePropriedade}' com valor ${valor} inválido. Valores aceitos:
+            número-quantificador,
+            ${Object.keys(valoresAceitos).reduce((final, atual) => final += `, ${atual}`)},
+            ${Object.keys(valoresExtra).reduce((final, atual) => final += `, ${atual}`)},
             ${Object.keys(valoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`)
         }
     }
