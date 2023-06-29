3aa520764516304296302adba512c49e030fa9e1
VitBrandao <vitorsbsilva@gmail.com>
2023-06-29 12:12:17 -0300

    Inclui novas funções em dicionario-valores

--- fontes/valores/dicionario-valores.ts
+++ fontes/valores/dicionario-valores.ts
@@ -1,13 +1,25 @@
+import { CubicBezier } from "./metodos/cubic-bezier";
+import { FitContent } from "./metodos/fit-content";
 import { Hex } from "./metodos/hex";
 import { Hsl } from "./metodos/hsl";
 import { Hsla } from "./metodos/hsla";
+import { Linear } from "./metodos/linear";
+import { MinMax } from "./metodos/minmax";
 import { Rgb } from "./metodos/rgb";
 import { Rgba } from "./metodos/rgba";
+import { Steps } from "./metodos/steps";
+import { Url } from "./metodos/url";
 
 export const DicionarioValores: { [nomeFolEs: string]: any } = {
-    "rgb": Rgb,
-    "rgba": Rgba,
+    "cubic-bezier": CubicBezier,
+    "fit-content": FitContent,
     "hsl": Hsl,
     "hsla": Hsla,
     "hex": Hex,
+    "linear": Linear,
+    "minmax": MinMax,
+    "rgb": Rgb,
+    "rgba": Rgba,
+    "steps": Steps,
+    "url": Url,
 }
