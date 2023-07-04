75a5d2e23419005bab33bba6a945adc90b582766
VitBrandao <vitorsbsilva@gmail.com>
2023-07-04 14:57:20 -0300

    Otimiza função de validação numérica para cobrir novas funções

--- fontes/modificadores/validacoes/numerica.ts
+++ fontes/modificadores/validacoes/numerica.ts
@@ -4,7 +4,7 @@ export function validarValorNumerico(
     nomePropriedade: string,
     valor: any,
     valoresAceitos?: { [valorFoles: string]: string },
-    valoresExtra?: { [valorFoles: string]: string }) 
+    valoresExtra?: any) 
 {
     if (valoresAceitos === undefined && valoresExtra === undefined) {
         if (Number.isNaN(parseInt(valor)) &&
@@ -28,28 +28,24 @@ export function validarValorNumerico(
         }
     }
 
-    if (valoresAceitos === undefined && valoresExtra !== undefined) {
-        if (Number.isNaN(parseInt(valor)) &&
-            !(valor in valoresExtra) &&
-            !(valor in valoresGlobais)
-        ) {
-            throw new Error(`Propriedade '${nomePropriedade}' com valor ${valor} inválido. Valores aceitos:
-            número-quantificador,
-            ${Object.keys(valoresExtra).reduce((final, atual) => final += `, ${atual}`)},
-            ${Object.keys(valoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`)
-        }
-    }
-
     if (valoresAceitos !== undefined && valoresExtra !== undefined) {
+        const valorString = valor.toString();
+        let metodoValido = false;
+        for (let index = 0; index < valoresExtra.length; index++) {
+            if(metodoValido === false) {
+                metodoValido = valorString.includes(valoresExtra[index]);
+            }
+        }
+
         if (Number.isNaN(parseInt(valor)) &&
             !(valor in valoresAceitos) &&
-            !(valor in valoresExtra) &&
+            !metodoValido &&
             !(valor in valoresGlobais)
         ) {
             throw new Error(`Propriedade '${nomePropriedade}' com valor ${valor} inválido. Valores aceitos:
             número-quantificador,
             ${Object.keys(valoresAceitos).reduce((final, atual) => final += `, ${atual}`)},
-            ${Object.keys(valoresExtra).reduce((final, atual) => final += `, ${atual}`)},
+            ${valoresExtra.reduce((final, atual) => final += `, ${atual}`)},
             ${Object.keys(valoresGlobais).reduce((final, atual) => final += `, ${atual}`)}.`)
         }
     }
