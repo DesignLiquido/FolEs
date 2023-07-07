3e6c3d4efe008ef128b477a625461a6029ba2a9f
VitBrandao <vitorsbsilva@gmail.com>
2023-07-07 11:23:28 -0300

    Criando testes unitários para os novos métodos

---
+++ testes/listas/metodos.ts
@@ -0,0 +1,52 @@
+export const MetodoEncaixarConteudo: Array<string> = [
+    'altura-maxima',
+    'altura-máxima',
+    'altura-minima',
+    'altura-mínima',
+    'altura',
+    'largura-maxima',
+    'largura-máxima',
+    'largura-minima',
+    'largura-mínima',
+    'largura',
+    'tamanho-colunas-em-grade',
+    'tamanho-em-linha',
+    'tamanho-linhas-em-grade',
+    'tamanho-maximo-em-bloco',
+    'tamanho-máximo-em-bloco',
+    'tamanho-maximo-em-linha',
+    'tamanho-máximo-em-linha',
+    'tamanho-mínimo-em-bloco',
+    'tamanho-mínimo-em-bloco',
+    'tamanho-minimo-em-linha',
+    'tamanho-mínimo-em-linha',
+];
+
+export const MetodoLimitar: Array<string> = [
+    'altura',
+];
+
+export const MetodoMinMax: Array<string> = [
+    'grade',
+    'tamanho-colunas-em-grade',
+    'tamanho-linhas-em-grade',
+];
+
+export const MetodoPassos: Array<string> = [
+    'tempo-transicao',
+    'tempo-transição',
+    'velocidade-animacao',
+    'velocidade-animação',
+];
+
+export const MetodoCurvaCubica: Array<string> = [
+    'tempo-transicao',
+    'tempo-transição',
+    'velocidade-animacao',
+    'velocidade-animação',
+];
+
+export const MetodoLinear: Array<string> = [
+    'transicao',
+    'transição',
+];
