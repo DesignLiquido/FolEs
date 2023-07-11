fb6704f7cb1011c546007c52fd5f8eefb86e5617
VitBrandao <vitorsbsilva@gmail.com>
2023-07-11 13:14:20 -0300

    Adiciona campo tradução a todos os novos métodos

--- fontes/valores/metodos/linear.ts
+++ fontes/valores/metodos/linear.ts
@@ -4,13 +4,15 @@ import { Metodo } from "./metodo";
 export class Linear extends Metodo {
     inicio: number;
     meio: number;
-    fim: number; 
+    fim: number;
+    traducao: string;
 
     constructor(inicio: Simbolo, meio: Simbolo, fim: Simbolo) {
         super();
         this.inicio = Number(inicio.lexema);
         this.meio = Number(meio.lexema);
         this.fim = Number(fim.lexema);
+        this.traducao = 'linear';
     }
 
     paraTexto() {
--- fontes/valores/metodos/minmax.ts
+++ fontes/valores/metodos/minmax.ts
@@ -3,8 +3,9 @@ import { Metodo } from "./metodo";
 export class MinMax extends Metodo {
     minimo: number | string;
     maximo: number | string;
+    traducao: string;
 
-    traducao = {
+    traducaoValores = {
         "conteudo-maximo": "max-content",
         "conteúdo-máximo": "max-content",
         "conteudo-minimo": "min-content",
@@ -14,8 +15,9 @@ export class MinMax extends Metodo {
 
     constructor(minimo: number | string, maximo: number | string) {
         super();
-        this.traducao[minimo] === undefined ? this.minimo = minimo : this.minimo = this.traducao[minimo];
-        this.traducao[maximo] === undefined ? this.maximo = maximo : this.maximo = this.traducao[maximo];
+        this.traducaoValores[minimo] === undefined ? this.minimo = minimo : this.minimo = this.traducaoValores[minimo];
+        this.traducaoValores[maximo] === undefined ? this.maximo = maximo : this.maximo = this.traducaoValores[maximo];
+        this.traducao = 'minmax';
     }
 
     paraTexto() {
