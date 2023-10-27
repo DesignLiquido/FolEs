cca316204e72dd4fb5a877fc17eea957d9746d1b
Leonel Sanches da Silva <leonel@designliquido.com.br>
2023-10-27 09:08:21 -0700

    Esboço do tradutor reverso.

---
+++ fontes/tradutores/tradutor-reverso.ts
@@ -0,0 +1,11 @@
+import { Declaracao } from "../declaracoes";
+
+export class TradutorReverso {
+    traduzir(declaracoes: Declaracao[]): Declaracao[] {
+        for (const declaracao of declaracoes) {
+            
+        }
+
+        return [];
+    }
+}
