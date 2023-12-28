45c31ab81a3562ff91fdd713ee7093f4154dc627
Vitor Brandão <82619724+VitBrandao@users.noreply.github.com>
2023-12-27 21:01:36 -0300

    Inicia lista de modificadores com valores específicos (#105)

---
+++ testes/listas/valores-especificos.ts
@@ -0,0 +1,57 @@
+// Múltiplos valores separados por vírgula
+// Ex.: área-máscara: visualizar-caixa, completar-caixa, borda-caixa;
+export const ValoresSeparadosPorVirgula: Array<string> = [
+    'área-máscara',
+    'atraso-transição',
+    'duração-animação',
+];
+
+// Múltiplos valores separados por barra
+// Ex.: coluna-em-grade: auto / auto;
+export const ValoresSeparadosPorBarra: Array<string> = [
+    'coluna-em-grade',
+    'cursor',
+    'linha-em-grade',
+    'proporção-tela',
+    'proporcao-tela',
+    'raio-borda',
+    'tamanho-grade',
+];
+
+// Valores do tipo <custom-ident>
+// Ex.: área-máscara: visualizar-caixa, completar-caixa, borda-caixa;
+export const ValoresCustomIdent: Array<string> = [
+    'ao-mudar',
+    'coluna-em-grade',
+    'definir-contador',
+    'estilo-lista-tipo',
+    'estilo-lista',
+    'fim-coluna-em-grade',
+    'fim-linha-em-grade',
+    'incrementar-contador',
+    'início-coluna-em-grade',
+    'inicio-coluna-em-grade',
+    'início-linha-em-grade',
+    'inicio-linha-em-grade',
+    'linha-em-grade',
+    'nome-animação',
+    'nome-animacao',
+    'propriedade-transição',
+    'propriedade-transicao',
+    'tamanho-grade',
+];
+
+// Valores do tipo string
+// Ex.: citações: "«" "»" "‹" "›";
+export const ValoresString: Array<string> = [
+    'citações',
+    'citacoes',
+    'configurações-variação-fonte',
+    'configuracoes-variacao-fonte',
+    'ênfase-texto',
+    'enfase-texto',
+    'estilo-ênfase-texto',
+    'estilo-enfase-texto',
+    'substituir-idioma-fonte',
+    'vazamento-texto',
+];
