5e0a4bcaddd0fe51a97b5495f28aad7df320f6e4
VitBrandao <vitorsbsilva@gmail.com>
2024-10-29 15:51:17 -0300

    Testes unitários de validação de cores e hexadecimais

--- testes/modificadores/validacoes-modificadores.test.ts
+++ testes/modificadores/validacoes-modificadores.test.ts
@@ -239,5 +239,137 @@ describe('Testando Validações de Valores e Quantificadores dos Seletores', () 
             }).toThrow(`Propriedade 'altura-máxima' com valor desconhecido inválido.`);
         });
     });
+
+    describe('Validação de cores e hexadecimais', () => {
+        let lexador: LexadorInterface;
+        let importador: ImportadorInterface;
+        let avaliador: AvaliadorSintaticoInterface;
+        let tradutor: Serializador;
+
+        beforeEach(() => {
+            lexador = new Lexador();
+            importador = new Importador(lexador);
+            avaliador = new AvaliadorSintatico(importador);
+            tradutor = new Serializador();
+        });
+
+        it('Caso de sucesso - Validações não retornam erros ao atribuir nome de cor', () => {
+            // Lexador
+            const resultadoLexador = lexador.mapear([
+                'divisão {',
+                    'cor-barra-rolagem: azul;',
+                "}"
+            ]);
+
+            // O Lexador deve montar um objeto de comprimento 7 sem retornar nenhum erro
+            expect(resultadoLexador.simbolos).toHaveLength(7);
+            expect(resultadoLexador.erros).toHaveLength(0);
+
+            // O Lexador deve mapear o tipo de símbolo Qualitativo
+            expect(resultadoLexador.simbolos).toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({ tipo: tiposDeSimbolos.QUALITATIVO }),
+                ])
+            );
+
+            // Avaliador Sintático
+            const resultadoAvaliadorSintatico = avaliador.analisar(resultadoLexador.simbolos);
+
+            // Serializador
+            const resultadoTradutor = tradutor.serializar(resultadoAvaliadorSintatico);
+
+            // O Serializador deve traduzir devidamente os termos
+            expect(resultadoTradutor).toContain('div');
+            expect(resultadoTradutor).toContain('scrollbar-color');
+            expect(resultadoTradutor).toContain('blue;');
+        });
+
+        it('Caso de sucesso - Validações não retornam erros ao atribuir código hexadecimal', () => {
+            // Lexador
+            const resultadoLexador = lexador.mapear([
+                'divisão {',
+                    'cor-barra-rolagem: #ffffff;',
+                "}"
+            ]);
+
+            // O Lexador deve montar um objeto de comprimento 8 sem retornar nenhum erro
+            expect(resultadoLexador.simbolos).toHaveLength(8);
+            expect(resultadoLexador.erros).toHaveLength(0);
+
+            // O Lexador deve mapear o tipo de símbolo Método
+            expect(resultadoLexador.simbolos).toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({ tipo: tiposDeSimbolos.METODO }),
+                ])
+            );
+
+            // Avaliador Sintático
+            const resultadoAvaliadorSintatico = avaliador.analisar(resultadoLexador.simbolos);
+
+            // Serializador
+            const resultadoTradutor = tradutor.serializar(resultadoAvaliadorSintatico);
+
+            // O Serializador deve traduzir devidamente os termos
+            expect(resultadoTradutor).toContain('div');
+            expect(resultadoTradutor).toContain('scrollbar-color');
+            expect(resultadoTradutor).toContain('#ffffff;');
+        });
+
+        it('Caso de falha - Validação retorna erro de código hexadecimal inválido', () => {
+            // Lexador
+            const resultadoLexador = lexador.mapear([
+                'divisão {',
+                    'cor-barra-rolagem: #f;',
+                "}"
+            ]);
+
+            // Avaliador Sintático
+            expect(() => {
+                avaliador.analisar(resultadoLexador.simbolos);
+            }).toThrow(`Propriedade 'cor-barra-rolagem' com hexadecimal inválido`);
+        });
+
+        it('Caso de falha - Validação retorna erro de método inválido', () => {
+            // Lexador
+            const resultadoLexador = lexador.mapear([
+                'divisão {',
+                    'cor-barra-rolagem: minmax(auto, 10px);',
+                "}"
+            ]);
+
+            // Avaliador Sintático
+            expect(() => {
+                avaliador.analisar(resultadoLexador.simbolos);
+            }).toThrow(`Propriedade 'cor-barra-rolagem' com método 'MinMax' inválido.`);
+        });
+
+        it('Caso de falha - Validação retorna erro de valor inválido', () => {
+            // Lexador
+            const resultadoLexador = lexador.mapear([
+                'divisão {',
+                    'cor-borda-direita: desconhecido;',
+                "}"
+            ]);
+
+            // Avaliador Sintático
+            expect(() => {
+                avaliador.analisar(resultadoLexador.simbolos);
+            }).toThrow(`Propriedade 'cor-borda-direita' com valor desconhecido inválido.`);
+        });
+
+        it('Caso de falha - Validação retorna erro de valor aceito, mas inválido', () => {
+            // Lexador
+            const resultadoLexador = lexador.mapear([
+                'divisão {',
+                    'cor-barra-rolagem: desconhecido;',
+                "}"
+            ]);
+
+            // Avaliador Sintático
+            expect(() => {
+                avaliador.analisar(resultadoLexador.simbolos);
+            }).toThrow(`Propriedade 'cor-barra-rolagem' com valor desconhecido inválido.`);
+        });
+    });
 });
 
