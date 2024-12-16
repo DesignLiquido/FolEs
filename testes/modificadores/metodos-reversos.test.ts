2ac5d1e40640d1eca104d0550e1ee154d348c54a
VitBrandao <vitorsbsilva@gmail.com>
2024-12-16 11:47:07 -0300

    Implementa testes unitarios do metodo brightness

--- testes/modificadores/metodos-reversos.test.ts
+++ testes/modificadores/metodos-reversos.test.ts
@@ -1,6 +1,6 @@
 import { Importador } from "../../fontes/importador";
 import { AvaliadorSintaticoInterface, ImportadorInterface, LexadorInterface } from "../../fontes/interfaces";
-import { MetodoBorrar, TraducaoValoresMetodos } from "../listas/metodos-css";
+import { MetodoBorrar, MetodoBrilho, TraducaoValoresMetodos } from "../listas/metodos-css";
 import tiposDeSimbolos from "../../fontes/tipos-de-simbolos/css";
 import { LexadorReverso } from "../../fontes/lexador/lexador-reverso";
 import { AvaliadorSintaticoReverso } from "../../fontes/avaliador-sintatico/avaliador-sintatico-reverso";
@@ -29,7 +29,7 @@ describe('Testando MÉTODOS no processo de tradução reversa', () => {
                     // Lexador
                     const resultadoLexador = lexador.mapear([
                         "div {",
-                        `${MetodoBorrar[index]}: blur(${valoresAceitos[valIndex]});`,
+                            `${MetodoBorrar[index]}: blur(${valoresAceitos[valIndex]});`,
                         "}"
                     ]);
                     
@@ -72,11 +72,70 @@ describe('Testando MÉTODOS no processo de tradução reversa', () => {
                     // Tradutor
                     const resultadoTradutor = tradutor.serializar(resultadoAvaliadorSintatico);
 
-                    // O Tradutor deve serializar de acordo e traduzir borrar para blur
+                    // O Tradutor deve serializar de acordo e traduzir blur para borrar
                     expect(resultadoTradutor).toContain(TraducaoValoresMetodos[MetodoBorrar[index]]);
                     expect(resultadoTradutor).toContain(`borrar(${valoresAceitos[valIndex]});`);
                 }
             }
         });
+
+        it('Atribuindo Método "brightness()"', () => {
+            for (let index = 0; index < MetodoBrilho.length; index += 1) {
+      
+              const valoresAceitos = ['100px', '100%', '0.1', '0', '1', '1.75'];
+      
+              for (let valIndex = 0; valIndex < valoresAceitos.length; valIndex += 1) {
+                // Lexador
+                const resultadoLexador = lexador.mapear([
+                  "div {",
+                        `${MetodoBrilho[index]}: brightness(${valoresAceitos[valIndex]});`,
+                  "}"
+                ]);
+      
+                // O Lexador não deve encontrar erros
+                expect(resultadoLexador.erros).toHaveLength(0);
+      
+                // O valor recebido deve ser mapeado como METODO
+                expect(resultadoLexador.simbolos).toEqual(
+                  expect.arrayContaining([
+                    expect.objectContaining({ tipo: tiposDeSimbolos.METODO }),
+                  ])
+                );
+      
+                // O Lexador deve montar um objeto de comprimento 11 caso haja quantificador e 10 caso não haja
+                if (valIndex === 0 || valIndex === 1) {
+                  expect(resultadoLexador.simbolos).toHaveLength(11);
+                  expect(resultadoLexador.simbolos).toEqual(
+                    expect.arrayContaining([
+                      expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
+                      expect.objectContaining({ tipo: tiposDeSimbolos.QUANTIFICADOR }),
+                    ])
+                  );
+                } else {
+                  expect(resultadoLexador.simbolos).toHaveLength(10);
+                  expect(resultadoLexador.simbolos).toEqual(
+                    expect.arrayContaining([
+                      expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
+                    ])
+                  );
+                }
+      
+                // Avaliador Sintático
+                const resultadoAvaliadorSintatico = avaliador.analisar(resultadoLexador.simbolos);
+      
+                // O Avaliador deve montar um objeto com os devidos nomes FolEs e CSS
+                expect(resultadoAvaliadorSintatico[0].modificadores[0].propriedadeCss).toStrictEqual(
+                    MetodoBrilho[index]
+                );
+      
+                // Tradutor
+                const resultadoTradutor = tradutor.serializar(resultadoAvaliadorSintatico);
+      
+                // O Tradutor deve serializar de acordo e traduzir brightness para brilho
+                expect(resultadoTradutor).toContain(TraducaoValoresMetodos[MetodoBrilho[index]]);
+                expect(resultadoTradutor).toContain(`brilho(${valoresAceitos[valIndex]});`);
+              }
+            }
+          });
     });
 });
