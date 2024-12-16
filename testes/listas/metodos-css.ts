0e0f905eb621932e44696eb80950ee11b70fc504
VitBrandao <vitorsbsilva@gmail.com>
2024-12-16 10:33:01 -0300

    Implementa listas de base para os testes unitarios dos metodos

---
+++ testes/listas/metodos-css.ts
@@ -0,0 +1,150 @@
+export const MetodoEncaixarConteudo: Array<string> = [
+    'max-height',
+    'min-height',
+    'height',
+    'max-width',
+    'min-width',
+    'width',
+    'grid-auto-columns',
+    'inline-size',
+    'grid-auto-rows',
+    'max-block-size',
+    'max-inline-size',
+    'min-block-size',
+    'min-inline-size',
+    'grid-template',
+];
+
+export const MetodoLimitar: Array<string> = [
+    'height',
+];
+
+export const MetodoMinMax: Array<string> = [
+    'grid',
+    'grid-auto-columns',
+    'grid-auto-rows',
+    'grid-template-columns',
+    'modelo-linhas-em-grade',
+];
+
+export const MetodoPassos: Array<string> = [
+    'transition-timing-function',
+    'animation-timing-function',
+];
+
+export const MetodoCurvaCubica: Array<string> = [
+    'transition-timing-function',
+    'animation-timing-function',
+];
+
+export const MetodoLinear: Array<string> = [
+    'transition',
+];
+
+export const MetodoCalcular: Array<string> = [
+    'gap',
+];
+
+export const MetodoGradienteLinear: Array<string> = [
+    'mask-image',
+    'imagem-máscara',
+    'border-image-source',
+    'content',
+];
+
+export const MetodoRaio: Array<string> = [
+    'offset',
+    'offset-path',
+];
+
+export const MetodoContraste: Array<string> = [
+    'filter',
+];
+
+export const MetodoBorrar: Array<string> = [
+    'filter',
+];
+
+export const MetodoBrilho: Array<string> = [
+    'filter',
+];
+
+export const MetodoEscalaCinza: Array<string> = [
+    'backdrop-filter',
+];
+
+export const MetodoInverter: Array<string> = [
+    'backdrop-filter',
+];
+
+export const MetodoOpacar: Array<string> = [
+    'backdrop-filter',
+];
+
+export const MetodoSaturar: Array<string> = [
+    'backdrop-filter',
+];
+
+export const MetodoSepia: Array<string> = [
+    'backdrop-filter',
+];
+
+export const MetodoRotacionarMatiz: Array<string> = [
+    'backdrop-filter',
+];
+
+export const MetodoProjetarSombra: Array<string> = [
+    'backdrop-filter',
+];
+
+export const MetodoPerspectivar: Array<string> = [
+    'transform',
+];
+
+export const MetodosRotacionar: Array<string> = [
+    'transform',
+];
+
+export const MetodosInclinar: Array<string> = [
+    'transform',
+];
+
+export const MetodosTranslacao: Array<string> = [
+    'transform',
+];
+
+export const MetodosEscalamento: Array<string> = [
+    'transform',
+];
+
+export const TraducaoValoresMetodos: Object = {
+    'max-height': 'altura-maxima',
+    'min-height': 'altura-minima',
+    'height': 'altura',
+    'content': 'conteudo',
+    'offset': 'deslocamento',
+    'gap': 'espacamento',
+    'filter': 'filtro',
+    'backdrop-filter': 'backdrop-filter',
+    'grid': 'grade',
+    'mask-image': 'imagem-mascara',
+    'max-width': 'largura-maxima', 
+    'min-width': 'largura-minima',
+    'width': 'largura',
+    'grid-template-columns': 'modelo-colunas-em-grade',
+    'grid-template': 'modelo-em-grade',
+    'grid-template-rows': 'modelo-linhas-em-grade',
+    'border-image-source': 'origem-imagem-borda',
+    'grid-auto-columns': 'tamanho-colunas-em-grade',
+    'inline-size': 'tamanho-em-linha',
+    'grid-auto-rows': 'tamanho-linhas-em-grade',
+    'max-block-size': 'tamanho-maximo-em-bloco',
+    'max-inline-size': 'tamanho-maximo-em-linha',
+    'min-block-size': 'tamanho-minimo-em-bloco', 
+    'min-inline-size': 'tamanho-minimo-em-linha',
+    'transition-timing-function': 'tempo-transicao',
+    'offset-path': 'trajeto-deslocamento',
+    'transform': 'transformar',
+    'transition': 'transicao',
+    'animation-timing-function': 'velocidade-animação',
+};
