e5d79fd5b6a173dae5e42aa1f6b10015c4cfce9f
VitBrandao <vitorsbsilva@gmail.com>
2025-01-09 14:48:13 -0300

    Exclui lógica de tradução de quantificadores no método rotate()

--- fontes/valores/metodos/css/rotate.ts
+++ fontes/valores/metodos/css/rotate.ts
@@ -15,7 +15,6 @@ export class Rotate extends MetodoCss {
 
     paraTexto() {
         if (this.quantificador) {
-            this.quantificador === 'graus' ? this.quantificador = 'deg' : null;
             return `rotacionar(${this.valor}${this.quantificador})`
         }
 
