a3ec2ebc0d4e61e26e8ca2ceb99cdb792068e299
VitBrandao <vitorsbsilva@gmail.com>
2025-02-17 19:20:36 -0300

    Cria classe SeletorVariavel para retorno do Avaliador Sintatico

---
+++ fontes/seletores/seletor-variavel.ts
@@ -0,0 +1,22 @@
+import { Pseudoclasse } from "../pseudoclasses/pseudoclasse";
+import { PragmasSeletor } from "./pragmas-seletor";
+import { Seletor } from "./seletor";
+
+export class SeletorVariavel extends Seletor {
+    nomeClasse: string;
+    pragmas?: PragmasSeletor;
+
+    constructor(nomeClasse: string, pseudoclasse?: Pseudoclasse, pragmas?: PragmasSeletor) {
+        super(pseudoclasse, pragmas);
+        this.nomeClasse = nomeClasse;
+    }
+
+    paraTexto() {
+        let resultado = `.${this.nomeClasse}`;
+        if (this.pseudoclasse !== undefined && this.pseudoclasse !== null) {
+            resultado += `:${this.pseudoclasse.pseudoclasseCss}`;
+        }
+        
+        return resultado;
+    }
+}
