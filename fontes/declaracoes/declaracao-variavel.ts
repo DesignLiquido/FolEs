f9a73120a50f6de2d71e45de95555408079c4ee8
VitBrandao <vitorsbsilva@gmail.com>
2025-03-06 14:34:27 -0300

    Exclui classe SeletorVariavel para seguir nova lógica

--- fontes/declaracoes/declaracao-variavel.ts
+++ fontes/declaracoes/declaracao-variavel.ts
@@ -1,6 +1,11 @@
 import { Metodo } from "../valores/metodos/foles/metodo";
 import { Declaracao } from "./declaracao";
 
+export interface DeclaracaoVariavelInterface {
+    nome: string;
+    valor: Metodo | string;
+}
+
 export class DeclaracaoVariavel extends Declaracao {
     nome: string;
     valor: Metodo | string;
--- fontes/declaracoes/declaracao.ts
+++ fontes/declaracoes/declaracao.ts
@@ -1,9 +1,12 @@
 import { Modificador } from "../modificadores";
 import { Seletor } from "../seletores";
+import { Metodo } from "../valores/metodos/foles/metodo";
 
 export abstract class Declaracao {
     seletores: Seletor[];
     modificadores: Modificador[];
     declaracoesAninhadas: Declaracao[];
     espacoReservado?: string;
+    nome: string;
+    valor: Metodo | string;
 }
