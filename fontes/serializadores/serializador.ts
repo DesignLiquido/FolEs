ce747b86b32047ce6ca53a5290f50bc200b4177c
VitBrandao <vitorsbsilva@gmail.com>
2025-03-12 18:39:27 -0300

    Ajusta Serializador para incluir o tratamento de variáveis

--- fontes/serializadores/serializador.ts
+++ fontes/serializadores/serializador.ts
@@ -6,6 +6,8 @@ import { SeletorEspacoReservado } from "../seletores/seletor-espaco-reservado";
 import { Metodo } from "../valores/metodos/foles/metodo";
 
 import estruturasHtml from "../tradutores/estruturas-html";
+import { DeclaracaoVariavel } from "../declaracoes/declaracao-variavel";
+import { Declaracao } from "../declaracoes/declaracao";
 
 /**
  * A classe que efetivamente traduz FolEs para CSS.
@@ -27,9 +29,8 @@ export class Serializador {
     ): string {
         // Caso 1: Número-Quantificador ou somente Número.
         if (Number(modificador.valor)) {
-            return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${
-                modificador.valor
-            }${modificador.quantificador || ""};\n`;
+            return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${modificador.valor
+                }${modificador.quantificador || ""};\n`;
         }
 
         // Caso 2: Tradução do valor contida no objeto 'valoresAceitos'.
@@ -39,23 +40,21 @@ export class Serializador {
         ) {
             const objetoValores = modificador["valoresAceitos"];
             const valorTraduzido = objetoValores[modificador.valor];
-            return `${" ".repeat(indentacao)}${
-                modificador.propriedadeCss
-            }: ${valorTraduzido};\n`;
+            return `${" ".repeat(indentacao)}${modificador.propriedadeCss
+                }: ${valorTraduzido};\n`;
         }
 
         // Caso 3: Valor é RGB, RGBA, HSL, HSLA ou HEX, ou seja, um método.
         if (modificador.valor instanceof Metodo) {
-            return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${
-                modificador.valor.paraTexto() || ""
-            };\n`;
+            return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${modificador.valor.paraTexto() || ""
+                };\n`;
         }
 
         // Caso 4: Atribuição Abreviada | Múltiplos valores separados por espaço, vírgula ou barra
         if (modificador.valor.includes(' ')) {
             if (modificador.valor.includes(',')) {
                 const separarValores: Array<string> = modificador.valor.split(', ');
-                
+
                 let valoresTraduzidos: string = '';
                 separarValores.forEach((valorIndividual: string, indexIndividual: number) => {
                     if (modificador["valoresAceitos"] && modificador["valoresAceitos"].hasOwnProperty(valorIndividual)) {
@@ -68,26 +67,22 @@ export class Serializador {
                 });
 
                 if (valoresTraduzidos.length !== 0) {
-                    return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${
-                        valoresTraduzidos
-                    };\n`;
+                    return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${valoresTraduzidos
+                        };\n`;
                 } else {
-                    return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${
-                        modificador.valor
-                    };\n`;
+                    return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${modificador.valor
+                        };\n`;
                 }
             }
 
-            return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${
-                modificador.valor
-            };\n`;
+            return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${modificador.valor
+                };\n`;
         }
-        
+
         // Caso 5: É um valor genérico, cuja tradução está na lista 'valoresGerais'.
         const valorTraduzido = valoresGerais[modificador.valor];
-        return `${" ".repeat(indentacao)}${
-            modificador.propriedadeCss
-        }: ${valorTraduzido};\n`;
+        return `${" ".repeat(indentacao)}${modificador.propriedadeCss
+            }: ${valorTraduzido};\n`;
     }
 
     /**
@@ -95,13 +90,14 @@ export class Serializador {
      * @param declaracoes As declaracoes.
      * @returns Uma string com o resultado da tradução.
      */
-    serializar(declaracoes: BlocoDeclaracao[], indentacao: number = 0, seletorAnterior: string = undefined) {
+    serializar(declaracoes: Declaracao[], indentacao: number = 0, seletorAnterior: string = undefined) {
         let resultado = "";
         let textoSeletorAnterior = "";
         if (seletorAnterior !== undefined) {
             textoSeletorAnterior = seletorAnterior;
         }
-        
+
+        declaracoes = declaracoes.filter((declaracao) => !(declaracao instanceof DeclaracaoVariavel));
 
         for (const declaracao of declaracoes) {
             const prefixos = [];
@@ -122,7 +118,7 @@ export class Serializador {
 
                         const traducaoSeletor = estruturasHtml[seletorSemPseudoclasse];
                         const traducaoPseudoclasse = seletor.pseudoclasse.pseudoclasseCss;
-                        
+
                         prefixo = (textoSeletorAnterior + " " + `${traducaoSeletor}:${traducaoPseudoclasse}`).trimStart();
                     } else {
                         const seletorLmht = seletor.paraTexto();
@@ -143,7 +139,7 @@ export class Serializador {
 
             resultado = resultado.slice(0, -2);
             resultado += " {\n";
-            
+
 
             for (const modificador of declaracao.modificadores) {
                 resultado += this.serializarModificador(
