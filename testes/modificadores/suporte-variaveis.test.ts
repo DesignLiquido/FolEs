67a9ea22f6192ace37c4acbc38fa5fb927dcb5ef
VitBrandao <vitorsbsilva@gmail.com>
2025-04-11 11:48:16 -0300

    Testes unitários - Metodos (suporte a variaveis)

--- testes/modificadores/suporte-variaveis.test.ts
+++ testes/modificadores/suporte-variaveis.test.ts
@@ -26,7 +26,7 @@ describe('Testando Suporte a VARIÁVEIS', () => {
             const resultadoLexador = lexador.mapear([
                 "$cor-secundaria: branco;",
                 "corpo {",
-                    "cor-barra-rolagem: $cor-secundaria;",
+                "cor-barra-rolagem: $cor-secundaria;",
                 "}"
             ]);
 
@@ -63,7 +63,7 @@ describe('Testando Suporte a VARIÁVEIS', () => {
             // O primeiro item do Avaliador deve ser a declaração de variável
             const primeiroResultado = resultadoAvaliadorSintatico[0];
             expect(primeiroResultado).toBeInstanceOf(DeclaracaoVariavel);
-            
+
             const primeiroResultadoTipado = primeiroResultado as DeclaracaoVariavel;
             expect(primeiroResultadoTipado.nome).toBe('cor-secundaria');
 
@@ -86,7 +86,7 @@ describe('Testando Suporte a VARIÁVEIS', () => {
             const resultadoLexador = lexador.mapear([
                 "$valor-indice: 0;",
                 "corpo {",
-                    "indice-z: $valor-indice;",
+                "indice-z: $valor-indice;",
                 "}"
             ]);
 
@@ -122,7 +122,7 @@ describe('Testando Suporte a VARIÁVEIS', () => {
             // O primeiro item do Avaliador deve ser a declaração de variável
             const primeiroResultado = resultadoAvaliadorSintatico[0];
             expect(primeiroResultado).toBeInstanceOf(DeclaracaoVariavel);
-            
+
             const primeiroResultadoTipado = primeiroResultado as DeclaracaoVariavel;
             expect(primeiroResultadoTipado.nome).toBe('valor-indice');
 
@@ -145,7 +145,7 @@ describe('Testando Suporte a VARIÁVEIS', () => {
             const resultadoLexador = lexador.mapear([
                 "$valor-recuo: 12px;",
                 "corpo {",
-                    "recuo-direito: $valor-recuo;",
+                "recuo-direito: $valor-recuo;",
                 "}"
             ]);
 
@@ -182,7 +182,7 @@ describe('Testando Suporte a VARIÁVEIS', () => {
             // O primeiro item do Avaliador deve ser a declaração de variável
             const primeiroResultado = resultadoAvaliadorSintatico[0];
             expect(primeiroResultado).toBeInstanceOf(DeclaracaoVariavel);
-            
+
             const primeiroResultadoTipado = primeiroResultado as DeclaracaoVariavel;
             expect(primeiroResultadoTipado.nome).toBe('valor-recuo');
 
@@ -198,6 +198,168 @@ describe('Testando Suporte a VARIÁVEIS', () => {
             expect(resultadoSerializador).toContain('body');
             expect(resultadoSerializador).toContain('padding-right');
             expect(resultadoSerializador).toContain('12px;');
+        });
+
+        it('Casos de sucesso - Atribuição de variável com MÉTODOS DE COR', () => {
+            const valoresMetodo = [
+                'rgb(31, 120, 50)',
+                'rgba(31, 120, 50)',
+                'hsl(50, 80%, 40%)',
+                'hsla(50, 80%, 40%)',
+            ]
+
+            for (let index = 0; index < valoresMetodo.length; index += 1) {
+                // Lexador
+                const resultadoLexador = lexador.mapear([
+                    `$cor-padrao: ${valoresMetodo[index]};`,
+                    "corpo {",
+                        "sombra-caixa: $cor-padrao;",
+                    "}"
+                ]);
+
+                // O Lexador deve montar um objeto de comprimento 14 ou 20 sem retornar nenhum erro
+                if (index <= 1) {
+                    expect(resultadoLexador.simbolos).toHaveLength(20);
+                } else {
+                    expect(resultadoLexador.simbolos).toHaveLength(22);
+                }
+                expect(resultadoLexador.erros).toHaveLength(0);
+
+                // O Lexador deve reconhecer a variável declarada
+                expect(resultadoLexador.simbolos).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({ tipo: tiposDeSimbolos.VARIAVEL }),
+                    ])
+                );
+
+                // O Lexador não deve encontrar qualitativos, números ou quantificadores na operação
+                expect(resultadoLexador.simbolos).not.toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({ tipo: tiposDeSimbolos.QUALITATIVO }),
+                        expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
+                        expect.objectContaining({ tipo: tiposDeSimbolos.QUANTIFICADOR }),
+                    ])
+                );
+
+                // O Lexador deve classificar o valor recebido como METODO
+                expect(resultadoLexador.simbolos).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({ tipo: tiposDeSimbolos.METODO }),
+                    ])
+                );
+
+                // Avaliador Sintático
+                const resultadoAvaliadorSintatico = avaliador.analisar(resultadoLexador.simbolos);
+                expect(resultadoAvaliadorSintatico.length).toBeGreaterThanOrEqual(1);
+
+                // O primeiro item do Avaliador deve ser a declaração de variável
+                const primeiroResultado = resultadoAvaliadorSintatico[0];
+                expect(primeiroResultado).toBeInstanceOf(DeclaracaoVariavel);
+
+                const primeiroResultadoTipado = primeiroResultado as DeclaracaoVariavel;
+                expect(primeiroResultadoTipado.nome).toBe('cor-padrao');
+
+                // O segundo item do Avaliador deve ser o bloco de declaração com os modificadores
+                const segundoResultado = resultadoAvaliadorSintatico[1];
+                expect(segundoResultado).toBeInstanceOf(BlocoDeclaracao);
+
+                const segundoResultadoTipado = segundoResultado as BlocoDeclaracao;
+                expect(segundoResultadoTipado.modificadores.length).toBeGreaterThanOrEqual(1);
+
+                // Serializador deve traduzir de acordo
+                const resultadoSerializador = tradutor.serializar(resultadoAvaliadorSintatico);
+                expect(resultadoSerializador).toContain('body');
+                expect(resultadoSerializador).toContain('box-shadow');
+                expect(resultadoSerializador).toContain(valoresMetodo[index]);
+            }
+        });
+
+        it('Casos de sucesso - Atribuição de variável com MÉTODOS GERAIS', () => {
+            const valoresMetodo = [
+                {
+                    foles: 'borrar(4px)',
+                    css: 'blur(4px)'
+                },
+                {
+                    foles: 'brilho(50%)',
+                    css: 'brightness(50%)'
+                },
+                {
+                    foles: 'contraste(50%)',
+                    css: 'contrast(50%)'
+                },
+                {
+                    foles: 'escala-cinza(30%)',
+                    css: 'grayscale(30%)'
+                },
+                {
+                    foles: 'rotacionar-matiz(90deg)',
+                    css: 'hue-rotate(90deg)'
+                },
+                {
+                    foles: 'inverter(50%)',
+                    css: 'invert(50%)'
+                },
+                {
+                    foles: 'opacar(50%)',
+                    css: 'opacity(50%)'
+                },
+                {
+                    foles: 'saturar(10%)',
+                    css: 'saturate(10%)'
+                },
+                {
+                    foles: 'sepia(60%)',
+                    css: 'sepia(60%)'
+                },
+            ]
+
+            for (let index = 0; index < valoresMetodo.length; index += 1) {
+                // Lexador
+                const resultadoLexador = lexador.mapear([
+                    `$metodo-padrao: ${valoresMetodo[index].foles};`,
+                    "corpo {",
+                        "filtro-fundo: $metodo-padrao;",
+                    "}"
+                ]);
+
+                // O Lexador deve montar um objeto de acordo, sem retornar nenhum erro
+                expect(resultadoLexador.erros).toHaveLength(0);
+
+                // O Lexador deve reconhecer a variável declarada, o método, o número e o quantificador
+                expect(resultadoLexador.simbolos).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({ tipo: tiposDeSimbolos.VARIAVEL }),
+                        expect.objectContaining({ tipo: tiposDeSimbolos.METODO }),
+                        expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
+                        expect.objectContaining({ tipo: tiposDeSimbolos.QUANTIFICADOR }),
+                    ])
+                );
+
+                // Avaliador Sintático
+                const resultadoAvaliadorSintatico = avaliador.analisar(resultadoLexador.simbolos);
+                expect(resultadoAvaliadorSintatico.length).toBeGreaterThanOrEqual(1);
+
+                // O primeiro item do Avaliador deve ser a declaração de variável
+                const primeiroResultado = resultadoAvaliadorSintatico[0];
+                expect(primeiroResultado).toBeInstanceOf(DeclaracaoVariavel);
+
+                const primeiroResultadoTipado = primeiroResultado as DeclaracaoVariavel;
+                expect(primeiroResultadoTipado.nome).toBe('metodo-padrao');
+
+                // O segundo item do Avaliador deve ser o bloco de declaração com os modificadores
+                const segundoResultado = resultadoAvaliadorSintatico[1];
+                expect(segundoResultado).toBeInstanceOf(BlocoDeclaracao);
+
+                const segundoResultadoTipado = segundoResultado as BlocoDeclaracao;
+                expect(segundoResultadoTipado.modificadores.length).toBeGreaterThanOrEqual(1);
+
+                // Serializador deve traduzir de acordo
+                const resultadoSerializador = tradutor.serializar(resultadoAvaliadorSintatico);
+                expect(resultadoSerializador).toContain('body');
+                expect(resultadoSerializador).toContain('backdrop-filter');
+                expect(resultadoSerializador).toContain(valoresMetodo[index].css);
+            }
         });
     });
 });
