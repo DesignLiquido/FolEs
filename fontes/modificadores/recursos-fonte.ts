3d63609616482ba2fa7ad1a5474317e60a393c77
VitBrandao <vitorsbsilva@gmail.com>
2025-04-22 11:37:33 -0300

    Permite atribuição de valores do tipo feature-tag-value

--- fontes/modificadores/recursos-fonte.ts
+++ fontes/modificadores/recursos-fonte.ts
@@ -1,5 +1,6 @@
 import { Modificador, PragmasModificador } from "./superclasse";
 import { validarValores } from "./validacoes/comum";
+import { validarValorString } from "./validacoes/string";
 
 export class RecursosFonte extends Modificador {
     valoresAceitos: { [valorFoles: string]: string } = {
@@ -14,8 +15,17 @@ export class RecursosFonte extends Modificador {
     ) {
         super("recursos-fonte", "font-feature-settings", pragmas);
 
-        if (!valorVariavel)
-            validarValores("recursos-fonte", valor, this.valoresAceitos);
+        const validacaoString = validarValorString(valor);
+
+        // Valor feature-tag-value: string de 4 caracteres (comprimento 6 com as aspas)
+        const valoresExtra = ["feature-tag-value"];
+        const validacaoTagValue = valor.length === 6;
+
+        if (validacaoString && validacaoTagValue) {
+            this.valoresAceitos[valor] = valor;
+        }
+
+        if (!valorVariavel) validarValores("recursos-fonte", valor, this.valoresAceitos, valoresExtra);
 
         this.valor = valor;
     }
