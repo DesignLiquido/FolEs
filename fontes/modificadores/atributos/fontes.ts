f65bdf4c20b19f3182b5e2dbcc0aca05333f9258
VitBrandao <vitorsbsilva@gmail.com>
2025-04-24 11:35:44 -0300

    Inclui listagem de fontes e cria função de validação de fontes

---
+++ fontes/modificadores/atributos/fontes.ts
@@ -0,0 +1,15 @@
+export const fontes: { [nomeFoles: string]: string } = {
+    // Fontes (family font name)
+    Arial: "Arial",
+    "Brush Script MT": "Brush Script MT ",
+    "Courier New": "Courier New",
+    Garamond: "Garamond",
+    "Gill Sans Extrabold": "Gill Sans Extrabold",
+    Georgia: "Georgia",
+    Helvetica: "Helvetica",
+    "Lucida Console": "Lucida Console",
+    Tahoma: "Tahoma",
+    "Trebuchet MS": "Trebuchet MS",
+    "Times New Roman": "Times New Roman",
+    Verdana: "Verdana",
+};
