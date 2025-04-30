8f3ebaf30f14ef107802a2e5f224801bb267db24
VitBrandao <vitorsbsilva@gmail.com>
2025-04-30 12:33:00 -0300

    Adapta Serializador para traduzir valores de atribuição abreviada

--- fontes/serializadores/serializador.ts
+++ fontes/serializadores/serializador.ts
@@ -82,73 +82,39 @@ export class Serializador {
             }
         }
 
-        // Caso 5: Atribuição Abreviada | Múltiplos valores separados por espaço, vírgula ou barra
+        // Caso 5: Atribuição Abreviada | Múltiplos valores separados por vírgula, barra ou espaço
         if (modificador.valor.includes(" ")) {
-            let valoresTraduzidos: string = "";
+            let separarValores: Array<string> = [];
+
             if (modificador.valor.includes(",")) {
-                const separarValores: Array<string> =
-                    modificador.valor.split(", ");
-
-                separarValores.forEach(
-                    (valorIndividual: string, indexIndividual: number) => {
-                        if (
-                            modificador["valoresAceitos"] &&
-                            modificador["valoresAceitos"].hasOwnProperty(
-                                valorIndividual,
-                            )
-                        ) {
-                            const objetoValores = modificador["valoresAceitos"];
-                            valoresTraduzidos += objetoValores[valorIndividual];
-                            if (indexIndividual < separarValores.length - 1) {
-                                valoresTraduzidos += ", ";
-                            }
-                        }
-
-                        if (valoresTraduzidos.length !== 0 && typeof modificador.valor === 'string') {
-                            modificador.valor = modificador.valor.replace(valorIndividual, "");
-                            modificador.valor += valoresTraduzidos;
-                        }
-                    }
-                );
-
-                // if (valoresTraduzidos.length !== 0) {
-                //     return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${valoresTraduzidos
-                //         };\n`;
-                // } else {
-                //     return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${modificador.valor
-                //         };\n`;
-                // }
-            } else {
-                const separarValores: Array<string> = modificador.valor.split(" ");
-
-                separarValores.forEach((valorIndividual, indexIndividual) => {
-                    if (
-                        modificador["valoresAceitos"] &&
-                        modificador["valoresAceitos"].hasOwnProperty(
-                            valorIndividual,
-                        )
-                    ) {
-                        const objetoValores = modificador["valoresAceitos"];
-                        valoresTraduzidos += objetoValores[valorIndividual];
-                        if (indexIndividual < separarValores.length - 1) {
-                            valoresTraduzidos += ", ";
-                        }
-                    } else if (valoresGerais[valorIndividual] !== undefined) {
-                        if (indexIndividual < separarValores.length - 1) {
-                            valoresTraduzidos += " ";
-                        }
-                        valoresTraduzidos += valoresGerais[valorIndividual];
-                    }
-
-                    if (valoresTraduzidos.length !== 0 && typeof modificador.valor === 'string') {
-                        modificador.valor = modificador.valor.replace(valorIndividual, "");
-                        modificador.valor += valoresTraduzidos;
-                    }
-                });
+                separarValores = modificador.valor.split(", ");
+            } else if (modificador.valor.includes("/")) {
+                separarValores = modificador.valor.split("/");
+            } else if (modificador.valor.includes(" ")) {
+                separarValores = modificador.valor.split(" ");
             }
 
-            return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${modificador.valor
-                };\n`;
+            separarValores.forEach((valorIndividual, indexIndividual) => {
+                let valoresTraduzidos: string = "";
+
+                if (
+                    modificador["valoresAceitos"] &&
+                    modificador["valoresAceitos"].hasOwnProperty(
+                        valorIndividual,
+                    )
+                ) {
+                    const objetoValores = modificador["valoresAceitos"];
+                    valoresTraduzidos += objetoValores[valorIndividual];
+                } else if (valoresGerais[valorIndividual] !== undefined) {
+                    valoresTraduzidos += valoresGerais[valorIndividual];
+                }
+
+                if (valoresTraduzidos.length !== 0 && typeof modificador.valor === 'string') {
+                    modificador.valor = modificador.valor.replace(valorIndividual, valoresTraduzidos);
+                }
+            });
+
+            return `${" ".repeat(indentacao)}${modificador.propriedadeCss}: ${modificador.valor};\n`;
         }
 
         // Caso 6: É um valor genérico, cuja tradução está na lista 'valoresGerais'.
