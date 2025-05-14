82db828d4650fd15b57b751312b4cba0f006b8e1
VitBrandao <vitorsbsilva@gmail.com>
2025-05-14 10:57:48 -0300

    Inclui valores numéricos com e sem quantificador nos testes de suporte a variáveis

--- testes/modificadores/valor-quantificador.test.ts
+++ testes/modificadores/valor-quantificador.test.ts
@@ -6,7 +6,7 @@ import { SeletorModificador } from "../../fontes/modificadores/superclasse";
 import tiposDeSimbolos from "../../fontes/tipos-de-simbolos/foles";
 import { Serializador } from "../../fontes/serializadores";
 import { ValorAngulo, ValorComprimento, ValorPercentual, ValorQuantificador, ValorTempo } from "../listas/valor-quantificador";
-import { BlocoDeclaracao } from "../../fontes/declaracoes";
+import { BlocoDeclaracao, DeclaracaoVariavel } from "../../fontes/declaracoes";
 
 describe('Testes: Valor-Quantificador', () => {
     let lexador: LexadorInterface;
@@ -89,6 +89,53 @@ describe('Testes: Valor-Quantificador', () => {
                 expect(() => {
                     avaliador.analisar(resultadoLexador.simbolos);
                 }).toThrow(`Esperado ';' após declaração de valor de modificador '${ValorQuantificador[index]}'.`);
+            }
+        });
+
+        it('Caso de Sucesso - Valor numérico atribuído por meio de variável', () => {
+            for (let index = 0; index < ValorQuantificador.length; index += 1) {
+                const seletor = new SeletorModificador(ValorQuantificador[index], '12', 'px');
+
+                // Lexador
+                const resultadoLexador = lexador.mapear([
+                    `$valor-padrao: 20px;`,
+                    "lmht {",
+                    `${ValorQuantificador[index]}: $valor-padrao;`,
+                    "}"
+                ]);
+
+                // O Lexador deve montar o objeto de acordo, sem retornar erros.
+                expect(resultadoLexador.simbolos).toHaveLength(14);
+                expect(resultadoLexador.erros).toHaveLength(0);
+
+                // O mapeamento deve conter o tipo variável
+                expect(resultadoLexador.simbolos).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({ tipo: tiposDeSimbolos.VARIAVEL }),
+                    ])
+                );
+
+                // Avaliador Sintático
+                const resultadoAvaliadorSintatico = avaliador.analisar(resultadoLexador.simbolos);
+
+                // O Avaliador deve montar um objeto com os devidos nomes FolEs e CSS
+                expect(resultadoAvaliadorSintatico.length).toBeGreaterThanOrEqual(1);
+
+                // O primeiro item do mapeamento deve ser a declaração da variável
+                const primeiroResultado = resultadoAvaliadorSintatico[0];
+                expect(primeiroResultado).toBeInstanceOf(DeclaracaoVariavel);
+
+                // O segundo item do mapeamento deve conter as devidas traduções
+                const segundoResultado = resultadoAvaliadorSintatico[1];
+                const segundoResultadoTipado = segundoResultado as BlocoDeclaracao;
+                expect(segundoResultadoTipado.modificadores.length).toBeGreaterThanOrEqual(1);
+                expect(segundoResultadoTipado.modificadores[0].nomeFoles).toStrictEqual(seletor['nomeFoles']);
+                expect(segundoResultadoTipado.modificadores[0].propriedadeCss).toStrictEqual(seletor['propriedadeCss']);
+
+                // Tradutor
+                const resultadoTradutor = tradutor.serializar(resultadoAvaliadorSintatico);
+                expect(resultadoTradutor).toContain(seletor['propriedadeCss']);
+                expect(resultadoTradutor).toContain('20px');
             }
         });
     });
