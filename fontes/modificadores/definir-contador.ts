a56334e2e27f8ab6640935aa27e4f6063f6e0f49
VitBrandao <vitorsbsilva@gmail.com>
2025-06-04 11:55:33 -0300

    Adapta primeiro modificador à nova lógica de validação personalizada

--- fontes/modificadores/definir-contador.ts
+++ fontes/modificadores/definir-contador.ts
@@ -1,5 +1,6 @@
 import { Modificador, PragmasModificador } from "./superclasse";
-import { validarValores } from "./validacoes/comum";
+import { validarAtribuicaoAbreviada } from "./validacoes/atribuicao-abreviada";
+import { validarValorNumerico } from "./validacoes/numerica";
 
 export class DefinirContador extends Modificador {
     valoresAceitos: { [valorFoles: string]: string } = {
@@ -13,9 +14,14 @@ export class DefinirContador extends Modificador {
         valorVariavel: boolean = false,
     ) {
         super("definir-contador", "counter-set", pragmas);
-
-        if (!valorVariavel)
-            validarValores("definir-contador", valor, this.valoresAceitos);
+        
+        if (!valorVariavel) {
+            if (valor.includes(" ")) {
+                validarAtribuicaoAbreviada("numérica", "definir-contador", valor, this.valoresAceitos, undefined, false, true);
+            } else {
+                validarValorNumerico("definir-contador", valor, this.valoresAceitos);
+            }
+        }
 
         this.valor = valor;
     }
