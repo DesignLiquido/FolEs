a573e321c2cd16a29e5f03a33cd015093a0c844b
VitBrandao <vitorsbsilva@gmail.com>
2025-06-04 15:46:01 -0300

    Implementa primeiros testes unitários de valores personalizados

---
+++ testes/listas/valores-personalizados.ts
@@ -0,0 +1,67 @@
+export const ValoresPersonalizados: Array<object> = [
+    {
+        modificador: 'ao-mudar',
+        valor: 'esquerda, direita',
+        css: 'will-change',
+    },
+    {
+        modificador: 'coluna-em-grade',
+        valor: 'span 3',
+        css: 'grid-column',
+    },
+    {
+        modificador: 'definir-contador',
+        valor: 'meu-contador 5 contador2 0',
+        css: 'counter-set',
+    },
+    {
+        modificador: 'estilo-lista',
+        valor: 'georgian lower-roman',
+        css: 'list-style',
+    },
+    {
+        modificador: 'fim-linha-em-grade',
+        valor: 'span 3',
+        css: 'grid-row-end',
+    },
+    {
+        modificador: 'fim-coluna-em-grade',
+        valor: 'span 3',
+        css: 'grid-column-end',
+    },
+    {
+        modificador: 'incrementar-contador',
+        valor: 'meu-contador -4',
+        css: 'counter-increment',
+    },
+    {
+        modificador: 'início-linha-em-grade',
+        valor: 'span 2',
+        css: 'grid-row-start',
+    },
+    {
+        modificador: 'início-coluna-em-grade',
+        valor: 'span 2',
+        css: 'grid-column-start'
+    },
+    {
+        modificador: 'linha-em-grade',
+        valor: 'span area-em-grade',
+        css: 'grid-row'
+    },
+    {
+        modificador: 'nome-animação',
+        valor: 'test_05',
+        css: 'animation-name',
+    },
+    {
+        modificador: 'propriedade-transição',
+        valor: 'test1, animacao4',
+        css: 'transition-property',
+    },
+    {
+        modificador: 'tamanho-grade',
+        valor: 'span 2',
+        css: 'grid-area',
+    },
+];
