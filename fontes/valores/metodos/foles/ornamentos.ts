4470dca7ce3c86fcc0a3b11cec35a8a65fc9b0ee
VitBrandao <vitorsbsilva@gmail.com>
2025-07-07 12:55:08 -0300

    Adapta ornamentos() e anotação() para receber valores string

--- fontes/valores/metodos/foles/ornamentos.ts
+++ fontes/valores/metodos/foles/ornamentos.ts
@@ -1,21 +1,27 @@
 import { Simbolo } from "../../../lexador";
+import { validarIdentificacaoPersonalizada } from "../../../modificadores/validacoes/identificacao-personalizada";
 import { Metodo } from "./metodo";
 
 export class Ornamentos extends Metodo {
-    valor: number;
+    valor: number | string;
     traducao: string;
 
     constructor(valor: Simbolo) {
         super();
-        this.valor = Number(valor.lexema);
+        this.valor = valor.tipo === 'NUMERO' ? Number(valor.lexema) : valor.lexema;
         this.traducao = "ornaments";
     }
 
     paraTexto() {
-        if (this.valor < 1 || this.valor > 99) {
-            throw new Error('O valor da função ornamentos() deve estar entre 1 e 99');
+        if (typeof this.valor === 'number') {
+            if (this.valor < 1 || this.valor > 99) {
+                throw new Error('O valor da função ornamentos() deve estar entre 1 e 99');
+            }
+            return `ornaments(${this.valor})`;
         }
 
+        const valorSemAspas = this.valor.replace(/^['"]|['"]$/g, '');
+        validarIdentificacaoPersonalizada('variação-fonte-alternativa', valorSemAspas);
         return `ornaments(${this.valor})`;
     }
 }
