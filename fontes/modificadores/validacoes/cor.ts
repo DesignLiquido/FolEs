b49c54fd6be75694c4dcb8969ada1de44ca54bca
VitBrandao <vitorsbsilva@gmail.com>
2025-07-31 15:14:45 -0300

    Conserta função de validação de valor Cor

--- fontes/modificadores/validacoes/cor.ts
+++ fontes/modificadores/validacoes/cor.ts
@@ -1,4 +1,4 @@
-import { Valor } from "../../valores";
+import { Valor, ValorQualitativo } from "../../valores";
 import { MetodoCss } from "../../valores/metodos/css/metodo-css";
 import { HexadecimalCor } from "../../valores/metodos/foles/hexadecimal-cor";
 import { Metodo } from "../../valores/metodos/foles/metodo";
@@ -9,59 +9,60 @@ export function validarValorCor(
     nomePropriedade: string,
     valores: Valor[],
     valoresAceitos?: { [valorFoles: string]: string },
-    valoresExtra?: { [valorFoles: string]: string },
 ) {
-    // TODO: Repensar.
-    // let metodoResolvido = "";
-    // if (valor instanceof Metodo) {
-    //     metodoResolvido = valor.traducao;
-    // } else if (valor instanceof MetodoCss) {
-    //     metodoResolvido = valor.traducao;
-    // } else {
-    //     metodoResolvido = valor;
-    // }
+    let valorModificador: string | number;
+    let valorTipoMetodo: boolean = false;
+    let valorTipado: any;
 
-    // if (valor instanceof Metodo) {
-    //     if (valor instanceof HexadecimalCor) {
-    //         if (valor["codigo"].length !== 3 && valor["codigo"].length !== 6) {
-    //             throw new Error(
-    //                 `Modificador ou variável '${nomePropriedade}' com hexadecimal inválido: '${valor["codigo"]}'. Hexadecimais
-    //                 devem ter 3 ou 6 caracteres após a cerquilha, sendo cada caracter de 0 até 9 ou de A até F.`,
-    //             );
-    //         }
-    //     } else if (
-    //         !["rgb", "rgba", "hsl", "hsla"].includes(
-    //             valor.constructor.name.toLowerCase(),
-    //         )
-    //     ) {
-    //         throw new Error(
-    //             `Modificador ou variável '${nomePropriedade}' com método '${valor.constructor.name}' inválido. Valores aceitos:
-    //             rgb(), rgba(), hsl(), hsla().`,
-    //         );
-    //     }
-    // } else {
-    //     // Cores pelo nome.
-    //     if (valoresAceitos === undefined) {
-    //         if (!(metodoResolvido in cores) && !(metodoResolvido in valoresGlobais)) {
-    //             throw new Error(
-    //                 `Modificador ou variável '${nomePropriedade}' com valor ${metodoResolvido} inválido. Valores aceitos:
-    //                 ${Object.keys(cores).reduce((final, atual) => (final += `, ${atual}`))},    
-    //                 ${Object.keys(valoresGlobais).reduce((final, atual) => (final += `, ${atual}`))}.`,
-    //             );
-    //         }
-    //     } else {
-    //         if (
-    //             !(metodoResolvido in cores) &&
-    //             !(metodoResolvido in valoresAceitos) &&
-    //             !(metodoResolvido in valoresGlobais)
-    //         ) {
-    //             throw new Error(
-    //                 `Modificador ou variável '${nomePropriedade}' com valor ${metodoResolvido} inválido. Valores aceitos:
-    //                     ${Object.keys(cores).reduce((final, atual) => (final += `, ${atual}`))},    
-    //                     ${Object.keys(valoresAceitos).reduce((final, atual) => (final += `, ${atual}`))},
-    //                     ${Object.keys(valoresGlobais).reduce((final, atual) => (final += `, ${atual}`))}.`,
-    //             );
-    //         }
-    //     }
-    // }
+    if (valores[0] instanceof ValorQualitativo) {
+        valorTipado = valores[0] as ValorQualitativo;
+        valorModificador = valorTipado.qualitativo;
+    } else if (valores[0] instanceof Metodo || valores[0] instanceof MetodoCss) {
+        valorTipado = valores[0] as Metodo;
+        valorModificador = valorTipado;
+        valorTipoMetodo = true;
+    }
+
+    if (valorTipoMetodo) {
+        if (valorTipado instanceof HexadecimalCor) {
+            if (valorModificador["codigo"].length !== 3 && valorModificador["codigo"].length !== 6) {
+                throw new Error(
+                    `Modificador ou variável '${nomePropriedade}' com hexadecimal inválido: '${valorModificador["codigo"]}'. Hexadecimais
+                    devem ter 3 ou 6 caracteres após a cerquilha, sendo cada caracter de 0 até 9 ou de A até F.`,
+                );
+            }
+        } else if (
+            !["rgb", "rgba", "hsl", "hsla"].includes(
+                valorModificador.constructor.name.toLowerCase(),
+            )
+        ) {
+            throw new Error(
+                `Modificador ou variável '${nomePropriedade}' com método '${valorModificador.constructor.name}' inválido. Valores aceitos:
+                rgb(), rgba(), hsl(), hsla().`,
+            );
+        }
+    } else {
+        if (valoresAceitos === null) {
+            if (!(valorModificador in cores) && !(valorModificador in valoresGlobais)) {
+                throw new Error(
+                    `Modificador ou variável '${nomePropriedade}' com valor ${valorModificador} inválido. Valores aceitos:
+                    ${Object.keys(cores).reduce((final, atual) => (final += `, ${atual}`))},    
+                    ${Object.keys(valoresGlobais).reduce((final, atual) => (final += `, ${atual}`))}.`,
+                );
+            }
+        } else {
+            if (
+                !(valorModificador in cores) &&
+                !(valorModificador in valoresAceitos) &&
+                !(valorModificador in valoresGlobais)
+            ) {
+                throw new Error(
+                    `Modificador ou variável '${nomePropriedade}' com valor ${valorModificador} inválido. Valores aceitos:
+                    ${Object.keys(cores).reduce((final, atual) => (final += `, ${atual}`))},    
+                    ${Object.keys(valoresAceitos).reduce((final, atual) => (final += `, ${atual}`))},
+                    ${Object.keys(valoresGlobais).reduce((final, atual) => (final += `, ${atual}`))}.`,
+                );
+            }
+        }
+    }
 }
