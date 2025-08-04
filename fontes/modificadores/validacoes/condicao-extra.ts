dd27a763f34449bd67acf7327ce2e38beb63958b
VitBrandao <vitorsbsilva@gmail.com>
2025-08-04 17:42:24 -0300

    Conserta validação de valores adicionais

--- fontes/modificadores/validacoes/condicao-extra.ts
+++ fontes/modificadores/validacoes/condicao-extra.ts
@@ -1,4 +1,4 @@
-import { Valor } from "../../valores";
+import { Valor, ValorQualitativo } from "../../valores";
 import { valoresGlobais } from "../atributos/globais";
 
 export function validarValoresAdicionais(
@@ -7,23 +7,25 @@ export function validarValoresAdicionais(
     valoresAdicionais: { [valorFoles: string]: string },
     valoresAceitos?: { [valorFoles: string]: string },
 ) {
-    // TODO: Repensar
-    // if (valoresAceitos !== undefined) {
-    //     if (
-    //         !(metodoResolvido in valoresAdicionais) &&
-    //         !(metodoResolvido in valoresAceitos) &&
-    //         !(metodoResolvido in valoresGlobais)
-    //     ) {
-    //         throw new Error(`Modificador ou variável '${nomePropriedade}' com valor ${valor} inválido. Valores aceitos:
-    //         ${Object.keys(valoresAdicionais).reduce((final, atual) => (final += `, ${atual}`))},
-    //         ${Object.keys(valoresAceitos).reduce((final, atual) => (final += `, ${atual}`))},
-    //         ${Object.keys(valoresGlobais).reduce((final, atual) => (final += `, ${atual}`))}.`);
-    //     }
-    // } else {
-    //     if (!(metodoResolvido in valoresAdicionais) && !(metodoResolvido in valoresGlobais)) {
-    //         throw new Error(`Modificador ou variável '${nomePropriedade}' com valor ${valor} inválido. Valores aceitos: 
-    //         ${Object.keys(valoresAdicionais).reduce((final, atual) => (final += `, ${atual}`))},
-    //         ${Object.keys(valoresGlobais).reduce((final, atual) => (final += `, ${atual}`))}.`);
-    //     }
-    // }
+    const valorTipado = valores[0] as ValorQualitativo;
+    const valorModificador: string | number = valorTipado.qualitativo;
+
+    if (valoresAceitos !== null) {
+        if (
+            !(valorModificador in valoresAdicionais) &&
+            !(valorModificador in valoresAceitos) &&
+            !(valorModificador in valoresGlobais)
+        ) {
+            throw new Error(`Modificador ou variável '${nomePropriedade}' com valor ${valorModificador} inválido. Valores aceitos:
+            ${Object.keys(valoresAdicionais).reduce((final, atual) => (final += `, ${atual}`))},
+            ${Object.keys(valoresAceitos).reduce((final, atual) => (final += `, ${atual}`))},
+            ${Object.keys(valoresGlobais).reduce((final, atual) => (final += `, ${atual}`))}.`);
+        }
+    } else {
+        if (!(valorModificador in valoresAdicionais) && !(valorModificador in valoresGlobais)) {
+            throw new Error(`Modificador ou variável '${nomePropriedade}' com valor ${valorModificador} inválido. Valores aceitos: 
+            ${Object.keys(valoresAdicionais).reduce((final, atual) => (final += `, ${atual}`))},
+            ${Object.keys(valoresGlobais).reduce((final, atual) => (final += `, ${atual}`))}.`);
+        }
+    }
 }
