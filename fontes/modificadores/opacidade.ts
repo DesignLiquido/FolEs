59b1fa9a8e7fbb0bddec30d75096b79e4affa37c
VitBrandao <vitorsbsilva@gmail.com>
2025-08-14 18:43:05 -0300

    Conserta lógica do modificador 'opacidade'

--- fontes/modificadores/opacidade.ts
+++ fontes/modificadores/opacidade.ts
@@ -3,7 +3,6 @@ import { valoresGlobais } from "./atributos/globais";
 import { ListaDeValorPercentual } from "./atributos/quantificadores";
 import { Modificador, PragmasModificador } from "./superclasse";
 import { validarValorNumerico } from "./validacoes/numerica";
-import { validarQuantificador } from "./validacoes/quantificador";
 
 export class Opacidade extends Modificador {
     constructor(
@@ -11,22 +10,22 @@ export class Opacidade extends Modificador {
         pragmas?: PragmasModificador,
     ) {
         super("opacidade", "opacity", pragmas);
-        
+
         // Valor numérico deve estar entre 0 e 1 (<alpha-value>).
-        // Caso haja um quantificador %, pode ser qualquer número.        
+        // Caso haja um quantificador (%), pode ser qualquer número.        
         const valorTipado = valores[0] as ValorNumerico;
 
-        if (valorTipado.literalNumerico > 1 && valorTipado.quantificador) {
+        if (valorTipado.quantificador) {
             validarValorNumerico(
-                "opacidade", 
+                "opacidade",
                 valores,
                 null,
                 null,
                 ListaDeValorPercentual
             );
-        } else {
+        } else if (valorTipado.literalNumerico >= 0 && valorTipado.literalNumerico <= 1) {
             validarValorNumerico(
-                "opacidade", 
+                "opacidade",
                 valores,
                 null,
                 null,
@@ -34,6 +33,11 @@ export class Opacidade extends Modificador {
                 null,
                 true
             )
+        } else {
+            throw new Error(`Modificador ou variável 'opacidade' com valor ${valorTipado.literalNumerico} inválido. Valores aceitos:
+            número-quantificador (ex.: 12px),
+            valor numérico do tipo <alpha-value> (deve ser entre 0 e 1),
+            ${Object.keys(valoresGlobais).reduce((final, atual) => (final += `, ${atual}`))}.`);
         }
 
         this.valores = valores;
