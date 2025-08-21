276f884bbe7f391a77e33b996e8ec201d6491ab0
VitBrandao <vitorsbsilva@gmail.com>
2025-08-21 13:28:59 -0300

    Complementa listas de modificadores AA com valores css

--- testes/listas/atribuicao-abreviada.ts
+++ testes/listas/atribuicao-abreviada.ts
@@ -1,91 +1,109 @@
 export const AtribuicaoAbreviadaVQePR: Array<object> = [
     {
         modificador: 'animacao',
+        css: 'animation',
         valor: '3s alternar 1s deslizar',
         traducao: '3s alternate 1s slide',
     },
     {
         modificador: 'animação',
+        css: 'animation',
         valor: '3s alternar 1s deslizar',
         traducao: '3s alternate 1s slide',
     },
     {
         modificador: 'borda',
+        css: 'border',
         valor: '2px pontilhado',
         traducao: '2px dotted',
     },
     {
         modificador: 'borda-direita',
+        css: 'border-right',
         valor: '2px pontilhado',
         traducao: '2px dotted',
     },
     {
         modificador: 'borda-em-bloco',
+        css: 'border-block',
         valor: '2px pontilhado',
         traducao: '2px dotted',
     },
     {
         modificador: 'borda-em-linha',
+        css: 'border-inline',
         valor: '2px pontilhado',
         traducao: '2px dotted',
     },
     {
         modificador: 'borda-esquerda',
+        css: 'border-left',
         traducao: '2px dotted',
         valor: '2px pontilhado',
     },
     {
         modificador: 'borda-inferior',
+        css: 'border-bottom',
         valor: '2px pontilhado',
         traducao: '2px dotted',
     },
     {
         modificador: 'borda-superior',
+        css: 'border-top',
         valor: '2px pontilhado',
         traducao: '2px dotted',
     },
     {
         modificador: 'colunas',
+        css: 'columns',
         valor: '6rem auto',
         traducao: '6rem auto',
     },
     {
         modificador: 'contorno',
+        css: 'outline',
         valor: '1rem grossa',
         traducao: '1rem thick',
     },
     {
         modificador: 'fim-borda-em-bloco',
+        css: 'border-block-end',
         valor: '2px pontilhado',
         traducao: '2px dotted',
     },
     {
         modificador: 'fim-borda-em-linha',
+        css: 'border-inline-end',
         valor: '2px pontilhado',
         traducao: '2px dotted',
     },
     {
         modificador: 'inicio-borda-em-bloco',
+        css: 'border-block-start',
         valor: '2px pontilhado',
         traducao: '2px dotted',
     },
     {
         modificador: 'início-borda-em-bloco',
+        css: 'border-block-start',
         valor: '2px pontilhado',
         traducao: '2px dotted',
     },
     {
         modificador: 'inicio-borda-em-linha',
+        css: 'border-inline-start',
         valor: '2px pontilhado',
         traducao: '2px dotted',
     },
     {
         modificador: 'início-borda-em-linha',
+        css: 'border-inline-start',
         valor: '2px pontilhado',
         traducao: '2px dotted',
     },
     {
         modificador: 'estilo-fonte',
+        css: 'font-style',
         valor: 'oblíqua 10deg',
         traducao: 'oblique 10deg',
     },
@@ -94,76 +112,91 @@ export const AtribuicaoAbreviadaVQePR: Array<object> = [
 export const AtribuicaoAbreviadaPREspecificas: Array<object> = [
     {
         modificador: 'ajustar-tamanho-fonte',
-        valor: 'altura-ex 0.5',
-        traducao: 'ex-height 0.5',
+        css: 'font-size-adjust',
+        valor: 'altura-ex 5',
+        traducao: 'ex-height 5',
     },
     {
         modificador: 'comportamento-rolagem-mouse',
+        css: 'overscroll-behavior',
         valor: 'auto conter',
         traducao: 'auto contain',
     },
     {
         modificador: 'cor-borda',
+        css: 'border-color',
         valor: 'vermelho amarelo verde azul',
         traducao: 'red yellow green blue',
     },
     {
         modificador: 'enfase-texto',
+        css: 'text-emphasis',
         valor: 'preenchido sesamo',
         traducao: 'filled sesame',
     },
     {
         modificador: 'flex-fluxo',
+        css: 'flex-flow',
         valor: 'inverter-linha não-agrupar',
         traducao: 'row-reverse nowrap',
     },
     {
         modificador: 'fonte',
-        valor: 'italica muito-pequeno negrito',
+        css: 'font',
+        valor: 'italica muito-pequeno em-negrito',
         traducao: 'italic x-small bold',
     },
     {
         modificador: 'fundo',
-        valor: 'borda repetir vermelho',
+        css: 'background',
+        valor: 'borda-caixa repetir vermelho',
         traducao: 'border-box repeat red',
     },
     {
         modificador: 'grade',
+        css: 'grid',
         valor: 'conteudo-maximo nenhum',
         traducao: 'max-content none',
     },
     {
         modificador: 'imagem-borda',
+        css: 'border-image',
         valor: 'preencher 27px',
         traducao: 'fill 27px',
     },
     {
         modificador: 'mascara',
+        css: 'mask',
         valor: 'conter alfa',
         traducao: 'contain alpha',
     },
     {
         modificador: 'posicionar-se',
+        css: 'place-self',
         valor: 'centro inicio',
         traducao: 'center start',
     },
     {
         modificador: 'sintetizar-fonte',
-        valor: 'maiúsculas-pequenas itálico',
+        css: 'font-synthesis',
+        valor: 'maiúsculas-pequenas itálica',
         traducao: 'small-caps style',
     },
     {
         modificador: 'transicao',
+        css: 'transition',
         valor: '4s inicio-final-lento',
         traducao: '4s ease-in-out',
     },
     {
         modificador: 'variacao-fonte',
+        css: 'font-variant',
         valor: 'ligacoes-comuns maiusculas-pequenas',
         traducao: 'common-ligatures small-caps',
     },
     {
         modificador: 'vazamento',
+        css: 'overflow',
         valor: 'auto visível',
         traducao: 'auto visible',
     }, 
@@ -211,30 +244,37 @@ export const AtribuicaoAbreviadaVQ: Array<string> = [
 export const AtribuicaoSeparadaPorBarra: Array<object> = [
     {
         modificador: 'coluna-em-grade',
+        css: 'grid-column',
         valor: 'auto / auto',
     },
     {
         modificador: 'tamanho-grade',
+        css: 'grid-area',
         valor: 'auto / auto / auto / auto',
     },
     {
         modificador: 'linha-em-grade',
+        css: 'grid-row',
         valor: '1 / 3',
     },
     {
         modificador: 'proporção-tela',
+        css: 'aspect-ratio',
         valor: '16 / 9',
     },
     {
         modificador: 'raio-borda',
+        css: 'border-radius',
         valor: '10px / 10px',
     },
     {
         modificador: 'modelo-em-grade',
+        css: 'grid-template',
         valor: '100px 1fr / 50px 1fr',
     },
     {
         modificador: 'imagem-borda',
+        css: 'border-image',
         valor: '27 / 35px',
     }
 ];
@@ -242,31 +282,37 @@ export const AtribuicaoSeparadaPorBarra: Array<object> = [
 export const AtribuicaoSeparadaPorVirgula: Array<object> = [
     {
         modificador: 'area-mascara',
+        css: 'mask-clip',
         valor: 'visualizar-caixa, completar-caixa, borda-caixa',
         traducao: 'view-box, fill-box, border-box',
     },
     {
         modificador: 'área-máscara',
+        css: 'mask-clip',
         valor: 'visualizar-caixa, completar-caixa, borda-caixa',
         traducao: 'view-box, fill-box, border-box',
     },
     {
         modificador: 'atraso-transicao',
+        css: 'transition-delay',
         valor: '2s, 4ms',
         traducao: '2s, 4ms',
     },
     {
         modificador: 'atraso-transição',
+        css: 'transition-delay',
         valor: '2s, 4ms',
         traducao: '2s, 4ms',
     },
     {
         modificador: 'duracao-animacao',
+        css: 'animation-duration',
         valor: '10s, 35s, 230ms',
         traducao: '10s, 35s, 230ms',
     },
     {
         modificador: 'duração-animação',
+        css: 'animation-duration',
         valor: '10s, 35s, 230ms',
         traducao: '10s, 35s, 230ms',
     },
