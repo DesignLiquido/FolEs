dc182f8e5ada6e8441581c21abab31c07497354a
VitBrandao <vitorsbsilva@gmail.com>
2025-08-25 14:54:11 -0300

    Conserta testes de Estilo

--- testes/modificadores/estilo.test.ts
+++ testes/modificadores/estilo.test.ts
@@ -27,8 +27,8 @@ describe('Testando Seletores com ESTILO como atributo', () => {
         it('Casos de sucesso - Valor válido', () => {
             for (let index = 0; index < Estilo.length; index += 1) {
                 const seletor = new SeletorModificador(
-                    Estilo[index], 
-                    [new ValorQualitativo('pontilhado')], 
+                    Estilo[index],
+                    [new ValorQualitativo('pontilhado')],
                     null
                 );
 
@@ -148,7 +148,7 @@ describe('Testando Seletores com ESTILO como atributo', () => {
             }
         });
 
-        // TODO: Consertar
+        // TODO: Consertar após ajustar processo de atribuição de valor via variável
         it.skip('Casos de sucesso - Valores de estilo atribuídos por meio de variável', () => {
             const estilosFolEs = Object.keys(estilos);
             const estilosCss = Object.values(estilos);
@@ -158,7 +158,7 @@ describe('Testando Seletores com ESTILO como atributo', () => {
                 const resultadoLexador = lexador.mapear([
                     `$estilo-padrao: ${estilosFolEs[index]};`,
                     "corpo {",
-                        'contorno: $estilo-padrao;',
+                    'contorno: $estilo-padrao;',
                     "}"
                 ]);
 
