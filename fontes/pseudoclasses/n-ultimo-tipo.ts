4e016b9421599df5b1ee9a94651b7ad99bd13eb1
VitBrandao <vitorsbsilva@gmail.com>
2025-10-03 14:55:27 -0300

    Ajustes para incluir pseudoclasses na listagem de tipos de simbolo

--- fontes/pseudoclasses/n-ultimo-tipo.ts
+++ fontes/pseudoclasses/n-ultimo-tipo.ts
@@ -2,6 +2,6 @@ import { Pseudoclasse, PragmasPseudoclasse } from "./pseudoclasse";
 
 export class NUltimoTipo extends Pseudoclasse {
     constructor(pragmas?: PragmasPseudoclasse) {
-        super("n-ultimo-tipo", "nth-of-type", pragmas);
+        super("n-ultimo-tipo", "nth-last-type", pragmas);
     }
 }
