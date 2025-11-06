755e8941bfd985e984062eaa473aa5e1bc8bff8f
VitBrandao <vitorsbsilva@gmail.com>
2025-11-06 15:09:52 -0300

    Implementa testes unitários do método reverso character-variant()

--- testes/modificadores/metodos-reversos.test.ts
+++ testes/modificadores/metodos-reversos.test.ts
@@ -79,7 +79,7 @@ describe('Testando MÉTODOS no processo de TRADUÇÃO REVERSA', () => {
             // Lexador
             const resultadoLexador = lexador.mapear([
                 "div {",
-                    `font-variant-alternates: annotation(${valoresAceitos[index]});`,
+                `font-variant-alternates: annotation(${valoresAceitos[index]});`,
                 "}"
             ]);
 
@@ -270,6 +270,76 @@ describe('Testando MÉTODOS no processo de TRADUÇÃO REVERSA', () => {
             const resultadoTradutor = resolvedor.resolver(resultadoAvaliadorSintatico);
             expect(resultadoTradutor).toContain(TraducaoValoresMetodos[MetodoCalcular[index]]);
             expect(resultadoTradutor).toContain('calcular(100px - 80px);');
+        }
+    });
+
+    it('Atribuindo Método "character-variant()" - caso de sucesso', () => {
+        const valoresAceitos = ['1', '2', '12', '20'];
+
+        for (let index = 0; index < valoresAceitos.length; index += 1) {
+            // Lexador
+            const resultadoLexador = lexador.mapear([
+                "div {",
+                `font-variant-alternates: character-variant(${valoresAceitos[index]});`,
+                "}"
+            ]);
+
+            // O Lexador não deve encontrar erros
+            expect(resultadoLexador.erros).toHaveLength(0);
+
+            // O valor recebido deve ser mapeado como METODO
+            expect(resultadoLexador.simbolos).toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({ tipo: tiposDeSimbolos.METODO }),
+                ])
+            );
+
+            // O Lexador deve montar um objeto de comprimento 10, incluindo mapeamento de valores numéricos
+            expect(resultadoLexador.simbolos).toHaveLength(10);
+            expect(resultadoLexador.simbolos).toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({ tipo: tiposDeSimbolos.NUMERO }),
+                ])
+            );
+
+            // Avaliador Sintático
+            const resultadoAvaliadorSintatico = avaliadorSintatico.analisar(resultadoLexador.simbolos);
+
+            // O Avaliador deve montar um objeto com os devidos nomes FolEs e CSS
+            expect(resultadoAvaliadorSintatico.length).toBeGreaterThanOrEqual(1);
+            const primeiroResultado = resultadoAvaliadorSintatico[0];
+            expect(primeiroResultado).toBeInstanceOf(BlocoDeclaracao);
+            const primeiroResultadoTipado = primeiroResultado as BlocoDeclaracao;
+            expect(primeiroResultadoTipado.modificadores.length).toBeGreaterThanOrEqual(1);
+            expect(primeiroResultadoTipado.modificadores[0].propriedadeCss).toStrictEqual(
+                'font-variant-alternates'
+            );
+
+            // Resolvedor
+            const resultadoResolvedor = resolvedor.resolver(resultadoAvaliadorSintatico);
+
+            // O Resolvedor deve resolver de acordo e traduzir character-variant para character-variant
+            expect(resultadoResolvedor).toContain('variacao-fonte-alternativa');
+            expect(resultadoResolvedor).toContain(`variar-caractere(${valoresAceitos[index]});`);
+        }
+    });
+
+    it('Atribuindo Método "character-variant()" - caso de falha', () => {
+        const valoresAceitos = ['0', '210', '100'];
+
+        for (let index = 0; index < valoresAceitos.length; index += 1) {
+            // Lexador
+            const resultadoLexador = lexador.mapear([
+                "div {",
+                `font-variant-alternates: character-variant(${valoresAceitos[index]});`,
+                "}"
+            ]);
+
+            const resultadoAvaliadorSintatico = avaliadorSintatico.analisar(resultadoLexador.simbolos);
+
+            expect(() => {
+                resolvedor.resolver(resultadoAvaliadorSintatico);
+            }).toThrow('O valor da função character-variant() deve estar entre 1 e 99');
         }
     });
 
