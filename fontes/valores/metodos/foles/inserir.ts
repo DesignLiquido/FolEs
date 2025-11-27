793b75615414fb7daae6e44cc61a8517f8b2c2b0
VitBrandao <vitorsbsilva@gmail.com>
2025-11-27 12:12:39 -0300

    Refatora métodos inserir() e inset() com lógica mais objetiva

--- fontes/valores/metodos/foles/inserir.ts
+++ fontes/valores/metodos/foles/inserir.ts
@@ -10,8 +10,9 @@ export class Inserir extends Metodo {
     quantificador3: string;
     valor4: number;
     quantificador4: string;
+    arrayValores: Array<number | string> = [];
     traducao: string;
-    valoresAceitos: {[nomeFolEs: string]: string};
+    valoresAceitos: { [nomeFolEs: string]: string };
 
     constructor(
         valor1: Simbolo, quantificador1: Simbolo,
@@ -32,6 +33,13 @@ export class Inserir extends Metodo {
         this.valor4 = valor4 ? Number(valor4.lexema) : null;
         this.quantificador4 = quantificador4 ? quantificador4.lexema : null;
 
+        this.arrayValores.push(
+            this.valor1, this.quantificador1,
+            this.valor2, this.quantificador2,
+            this.valor3, this.quantificador3,
+            this.valor4, this.quantificador4,
+        );
+
         this.traducao = "inset";
 
         this.valoresAceitos = {
@@ -40,14 +48,22 @@ export class Inserir extends Metodo {
     }
 
     paraTexto() {
-        if (this.valor4) {
-            return `inset(${this.valor1}${this.quantificador1} ${this.valor2}${this.quantificador2} ${this.valor3}${this.quantificador3} ${this.valor4}${this.quantificador4})`;
-        } else if (this.valor3) {
-            return `inset(${this.valor1}${this.quantificador1} ${this.valor2}${this.quantificador2} ${this.valor3}${this.quantificador3})`;
-        } else if (this.valor2) {
-            return `inset(${this.valor1}${this.quantificador1} ${this.valor2}${this.quantificador2})`;
-        }
+        let traducaoRetorno: string = '';
 
-        return `inset(${this.valor1}${this.quantificador1})`;
+        this.arrayValores.forEach((valorIndividual, index) => {
+            if (valorIndividual) {
+                if (typeof valorIndividual === 'number') {
+                    if (index === 0) {
+                        traducaoRetorno += `${valorIndividual}`;
+                    } else {
+                        traducaoRetorno += ` ${valorIndividual}`;
+                    }
+                } else {
+                    traducaoRetorno += `${valorIndividual}`;
+                }
+            }
+        });
+
+        return `inset(${traducaoRetorno})`;
     }
 }
