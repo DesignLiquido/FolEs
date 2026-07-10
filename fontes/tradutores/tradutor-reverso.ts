5f7ba88c010162c9a628ce794bac44478d836115
VitBrandao <vitorsbsilva@gmail.com>
2026-07-10 14:02:22 -0300

    Adaptando lógica do Tradutor Reverso às novas classes

--- fontes/tradutores/tradutor-reverso.ts
+++ fontes/tradutores/tradutor-reverso.ts
@@ -1,4 +1,5 @@
-import { BlocoDeclaracao } from "../declaracoes";
+import { BlocoDeclaracao, DeclaracaoVariavel } from "../declaracoes";
+import { Estrutura } from "../estruturas/estrutura";
 import { SeletorEstruturasHtml } from "../estruturas/seletor-estruturas-html";
 import { Modificador } from "../modificadores";
 import {
@@ -43,10 +44,16 @@ export class TradutorReverso {
     private traduzirSeletorEstrutura(seletor: SeletorEstrutura): Seletor {
         const estruturaHtml = seletor.estrutura.tagHtml;
         const novosPragmasSeletor = this.calcularPragmasSeletor(estruturaHtml);
-        return new SeletorEstruturasHtml(
+        const estruturaTraduzida = new SeletorEstruturasHtml(
             estruturaHtml,
             novosPragmasSeletor,
-        ) as Seletor;
+        ) as Estrutura;
+        const seletorTraduzido = new SeletorEstrutura(
+            estruturaTraduzida,
+            seletor.pseudoclasse,
+        );
+
+        return seletorTraduzido;
     }
 
     traduzir(declaracoes: BlocoDeclaracao[]): BlocoDeclaracao[] {
@@ -59,37 +66,43 @@ export class TradutorReverso {
             const seletoresTraduzidos: Seletor[] = [];
             const modificadoresTraduzidos: Modificador[] = [];
 
-            for (const seletor of declaracao.seletores) {
-                this.linha = seletor.pragmas.linha;
+            if (declaracao instanceof BlocoDeclaracao) {
+                for (const seletor of declaracao.seletores) {
+                    this.linha = seletor.pragmas.linha;
 
-                if (seletor instanceof SeletorEstrutura) {
-                    seletoresTraduzidos.push(
-                        this.traduzirSeletorEstrutura(seletor),
-                    );
-                    continue;
+                    if (seletor instanceof SeletorEstrutura) {
+                        seletoresTraduzidos.push(
+                            this.traduzirSeletorEstrutura(seletor),
+                        );
+                        continue;
+                    }
+
+                    seletoresTraduzidos.push(seletor);
                 }
-            }
 
-            for (const modificador of declaracao.modificadores) {
-                this.linha = modificador.pragmas.linha;
-                modificadoresTraduzidos.push(
-                    new SeletorModificador(
-                        Array.isArray(modificador.nomeFoles)
-                            ? modificador.nomeFoles[0]
-                            : modificador.nomeFoles,
-                        modificador.valores,
-                        this.calcularPragmasModificador(modificador),
-                    ) as Modificador,
+                for (const modificador of declaracao.modificadores) {
+                    this.linha = modificador.pragmas.linha;
+                    modificadoresTraduzidos.push(
+                        new SeletorModificador(
+                            Array.isArray(modificador.nomeFoles)
+                                ? modificador.nomeFoles[0]
+                                : modificador.nomeFoles,
+                            modificador.valores,
+                            this.calcularPragmasModificador(modificador),
+                        ) as Modificador,
+                    );
+                }
+
+                declaracoesTraduzidas.push(
+                    new BlocoDeclaracao(
+                        seletoresTraduzidos,
+                        modificadoresTraduzidos,
+                        [],
+                    ),
                 );
+            } else {
+                declaracoesTraduzidas.push(declaracao)
             }
-
-            declaracoesTraduzidas.push(
-                new BlocoDeclaracao(
-                    seletoresTraduzidos,
-                    modificadoresTraduzidos,
-                    [],
-                ),
-            );
         }
 
         return declaracoesTraduzidas;
